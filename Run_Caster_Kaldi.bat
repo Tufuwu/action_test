ebd5ccb176eec9a852b20af3b134cefcc9a893bc
wu <wu@.com>
2025-03-17 01:05:25 +0800

    tue-robotics/image_recognition/action

--- Run_Caster_Kaldi.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Running Kaldi from Dragonfly CLI
-
-set currentpath=%~dp0
-
-TITLE Caster: Status Window
-python -m dragonfly load _*.py --engine kaldi  --no-recobs-messages
-
-pause 1
--- castervoice/lib/printer.py
+++
@@ -1,6 +0,0 @@
-def out(*args):
-    """
-    Use this as a printing interface to send messages to places other than the console.
-    DO NOT import anything in this class. Use *args.
-    """
-    print("\n".join([str(o) for o in args]))
