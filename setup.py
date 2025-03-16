e8a0cb0ef5c1a20baabd02c4050f02c4a0293522
wu <wu@.com>
2025-03-17 01:55:38 +0800

    eliben/pss/action

--- setup.py
+++ setup.py
@@ -1,62 +1,63 @@
-#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+# pss: setup.py
+#
+# Setup/installation script.
+#
+# Eli Bendersky (eliben@gmail.com)
+# This code is in the public domain
+#-------------------------------------------------------------------------------
+import os, sys
+try:
+    from setuptools import setup
+    use_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    use_setuptools = False
 
-import glob
-from setuptools import setup, find_packages
+if use_setuptools:
+    # Setuptools provides an "entry points" facility to automatically generate
+    # scripts that work in the same way as the supplied "pss" script. This
+    # feature is more portable to other platforms than providing a manually
+    # created script, so we use that feature if it is available.
+    # By using entry points, we get a "pss" shell script on Unix, and a
+    # "pss.exe" command on Windows, without any extra effort.
+    extra_args = {
+        'entry_points': {
+            'console_scripts': 'pss = psslib.pss:main'
+        },
+    }
+else:
+    # Setuptools is not available, so fall back to custom built scripts.
+    extra_args = {
+        'scripts': ['scripts/pss.py', 'scripts/pss'],
+    }
 
-install_requires = []
-tests_require = ["coverage", "flake8", "pexpect", "wheel"]
-importlib_backport_requires = ["importlib-metadata >= 0.23, < 3"]
+
+try:
+    with open('README.rst', 'rt') as readme:
+        description = '\n' + readme.read()
+except IOError:
+    # maybe running setup.py from some other dir
+    description = ''
+
 
 setup(
-    name='argcomplete',
-    version='1.12.1',
-    url='https://github.com/kislyuk/argcomplete',
-    project_urls={
-        "Documentation": "https://kislyuk.github.io/argcomplete",
-        "Source Code": "https://github.com/kislyuk/argcomplete",
-        "Issue Tracker": "https://github.com/kislyuk/argcomplete/issues"
-    },
-    license='Apache Software License',
-    author='Andrey Kislyuk',
-    author_email='kislyuk@gmail.com',
-    description='Bash tab completion for argparse',
-    long_description=open('README.rst').read(),
-    install_requires=install_requires,
-    tests_require=tests_require,
-    extras_require={
-        "test": tests_require,
-        ':python_version == "2.7"': importlib_backport_requires,
-        ':python_version == "3.5"': importlib_backport_requires,
-        ':python_version == "3.6"': importlib_backport_requires,
-        ':python_version == "3.7"': importlib_backport_requires
-    },
-    packages=find_packages(exclude=['test']),
-    scripts=glob.glob('scripts/*'),
-    package_data={'argcomplete': ['bash_completion.d/python-argcomplete']},
-    zip_safe=False,
-    include_package_data=True,
-    platforms=['MacOS X', 'Posix'],
-    test_suite='test',
-    classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
+    # metadata
+    name='pss',
+    description='Tool for grepping through source code',
+    long_description=description,
+    license='Public domain',
+    version='1.42',
+    author='Eli Bendersky',
+    maintainer='Eli Bendersky',
+    author_email='eliben@gmail.com',
+    url='https://github.com/eliben/pss',
+    platforms='Cross Platform',
+    classifiers = [
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Development Status :: 5 - Production/Stable',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Shells',
-        'Topic :: Terminals'
-    ]
+        'Programming Language :: Python :: 3',],
+
+    packages=['psslib', 'psslib.colorama'],
+
+    **extra_args
 )
