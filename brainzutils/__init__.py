6e7dfa14fadc4a2628b23ee1a82020948f344637
wu <wu@.com>
2025-03-16 22:49:23 +0800

    Josh-XT/pytube2/action

--- brainzutils/__init__.py
+++
@@ -1,9 +0,0 @@
-# TODO: We are the backport of importlib to support python 3.7.
-#  When we raise the minimum to python 3.8, we can remove this and use the builtin importlib module.
-from importlib_metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    # package is not installed
-    pass
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose -f test/docker-compose.test.yml -p brainzutils_test run --rm test
