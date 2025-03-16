1eaf8ae19c60c3b6a4210c65a511d96c3cf757c5
wu <wu@.com>
2025-03-16 22:38:46 +0800

    percipient/django-allauth-2fa/action

--- setup.py
+++ setup.py
@@ -1,64 +1,49 @@
-import os
+# -*- coding: utf-8 -*-
 
-from setuptools import setup
+import codecs
 
-
-def rel(*xs):
-    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *xs)
+from setuptools import find_packages, setup
 
 
-with open(rel("django_dramatiq", "__init__.py"), "r") as f:
-    version_marker = "__version__ = "
-    for line in f:
-        if line.startswith(version_marker):
-            _, version = line.split(version_marker)
-            version = version.strip().strip('"')
-            break
-    else:
-        raise RuntimeError("Version marker not found.")
+def long_description():
+    with codecs.open('README.rst', encoding='utf8') as f:
+        return f.read()
 
 
 setup(
-    name="django_dramatiq",
-    version=version,
-    description="A Django app for Dramatiq.",
-    long_description="Visit https://github.com/Bogdanp/django_dramatiq for more information.",
-    packages=[
-        "django_dramatiq",
-        "django_dramatiq.management",
-        "django_dramatiq.management.commands",
-        "django_dramatiq.migrations",
-    ],
-    install_requires=[
-        "django>=2.2",
-        "dramatiq>=0.18.0",
-    ],
-    classifiers=[
-        "Environment :: Web Environment",
-        "Operating System :: OS Independent",
-        "Framework :: Django",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.1",
-        "Framework :: Django :: 3.2",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    extras_require={
-        "dev": [
-            "bumpversion",
-            "flake8",
-            "flake8-quotes",
-            "isort",
-            "pytest",
-            "pytest-cov",
-            "pytest-django",
-            "tox",
-            "twine",
-        ]
-    },
-    python_requires=">=3.5",
+    name="django-allauth-2fa",
+    version="0.8",
+    packages=find_packages('.', include=('allauth_2fa', 'allauth_2fa.*')),
     include_package_data=True,
+    install_requires=[
+        "django>=1.11",
+        "qrcode>=5.3",
+        "django-allauth>=0.25",
+        "django-otp>=0.3.12",
+    ],
+    author="Víðir Valberg Guðmundsson",
+    author_email="valberg@orn.li",
+    description="Adds two factor authentication to django-allauth",
+    license="Apache 2.0",
+    keywords=['otp', 'auth', 'two factor authentication', 'allauth', 'django', '2fa'],
+    url="https://github.com/percipient/django-allauth-2fa",
+    long_description=long_description(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Web Environment',
+        'Topic :: Internet',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    python_requires=">=3.6",
 )
