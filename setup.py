13ad046aad444b85102678b8bb6d926f8c5c933d
wu <wu@.com>
2025-03-16 19:03:08 +0800

    pogzyb/whodap/action

--- setup.py
+++ setup.py
@@ -1,41 +1,48 @@
-import os
+from setuptools import setup
+import pathlib
 
-from setuptools import setup, find_packages
 
-from nrrd._version import __version__
+base_dir = pathlib.Path(__file__).parent.resolve()
+long_description = (base_dir / 'README.md').read_text(encoding='utf-8')
 
-currentPath = os.path.abspath(os.path.dirname(__file__))
 
-# Get the long description from the README file
-with open(os.path.join(currentPath, 'README.rst'), 'r') as fh:
-    longDescription = fh.read()
+def get_version(location: str) -> str:
+    with open((base_dir / location).absolute().resolve()) as file:
+        for line in file.readlines():
+            if line.startswith('__version__'):
+                return line.split(' = ')[-1].strip().replace("'", "")
+        else:
+            raise RuntimeError('Unable to find version string.')
 
-longDescription = '\n' + longDescription
 
-setup(name='pynrrd',
-      python_requires='>=3.7',
-      version=__version__,
-      description='Pure python module for reading and writing NRRD files.',
-      long_description=longDescription,
-      long_description_content_type='text/x-rst',
-      author='Maarten Everts',
-      author_email='me@nn8.nl',
-      url='https://github.com/mhe/pynrrd',
-      license='MIT License',
-      install_requires=['numpy>=1.11.1'],
-      packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
-      keywords='nrrd teem image processing file format',
-      classifiers=[
-          'License :: OSI Approved :: MIT License',
-          'Topic :: Scientific/Engineering',
-          'Programming Language :: Python',
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
-          "Programming Language :: Python :: 3.10",
-          "Programming Language :: Python :: 3.11"
-      ],
-      project_urls={
-          'Tracker': 'https://github.com/mhe/pynrrd/issues',
-      }
-      )
+setup(
+    name='whodap',
+    version=get_version('whodap/__init__.py'),
+    description='Simple RDAP Utility for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pogzyb/whodap',
+    author='Joseph Obarzanek',
+    author_email='pogzyb@umich.edu',
+    python_requires='>=3.6, <4',
+    keywords='security, whois, rdap, research',
+    install_requires=[
+        'httpx>=0.20.0',
+        'async_generator>=1.10; python_version < "3.7.0"'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Security',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=['whodap'],
+    package_dir={'whodap': 'whodap'},
+)
