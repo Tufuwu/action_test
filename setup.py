7f4c5cadecd331dfdc6827733aeddc154ad7579c
wu <wu@.com>
2025-03-17 01:10:00 +0800

    enjoy-digital/litedram/action

--- setup.py
+++ setup.py
@@ -5,20 +5,21 @@ from setuptools import find_packages
 
 
 setup(
-    name="litescope",
-    description="Small footprint and configurable embedded FPGA logic analyzer core",
+    name="litedram",
+    description="Small footprint and configurable DRAM core",
     author="Florent Kermarrec",
     author_email="florent@enjoy-digital.fr",
     url="http://enjoy-digital.fr",
-    download_url="https://github.com/enjoy-digital/litescope",
+    download_url="https://github.com/enjoy-digital/litedram",
     test_suite="test",
     license="BSD",
     python_requires="~=3.6",
+    install_requires=["pyyaml"],
     packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
     include_package_data=True,
     entry_points={
         "console_scripts": [
-            "litescope_cli=litescope.software.litescope_cli:main",
+            "litedram_gen=litedram.gen:main",
         ],
     },
 )
--- test/test_examples.py
+++ test/test_examples.py
@@ -1,19 +1,31 @@
 #
-# This file is part of LiteScope.
+# This file is part of LiteDRAM.
 #
-# Copyright (c) 2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2018-2019 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
 import unittest
 import os
 
-from litescope.software.dump import *
 
-root_dir    = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
+def build_config(name):
+    errors = 0
+    os.system("rm -rf examples/build")
+    os.system("cd examples && python3 ../litedram/gen.py {}.yml".format(name))
+    errors += not os.path.isfile("examples/build/gateware/litedram_core.v")
+    os.system("rm -rf examples/build")
+    return errors
+
 
 class TestExamples(unittest.TestCase):
     def test_arty(self):
-        os.system(f"rm -rf {root_dir}/build")
-        os.system(f"cd {root_dir}/examples && python3 arty.py")
-        self.assertEqual(os.path.isfile(f"{root_dir}/examples/build/arty/gateware/arty.v"), True)
+        errors = build_config("arty")
+        self.assertEqual(errors, 0)
 
+    def test_nexys4ddr(self):
+        errors = build_config("nexys4ddr")
+        self.assertEqual(errors, 0)
+
+    def test_genesys2(self):
+        errors = build_config("genesys2")
+        self.assertEqual(errors, 0)
