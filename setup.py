8c36e8b7c42cf779f28527619b62d3aa1b2d7905
wu <wu@.com>
2025-03-16 21:54:01 +0800

    aiven/pghoard/action

--- setup.py
+++ setup.py
@@ -1,81 +1,62 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-"""
-
-import io
-from os import path
-
-try:
-    from pip.req import parse_requirements
-except ImportError:
-    # pip >= 10
-    from pip._internal.req import parse_requirements
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+import os
+import version
 
 
-def get_requirements(requirements_file):
-    """Use pip to parse requirements file."""
-    requirements = []
-    if path.isfile(requirements_file):
-        for req in parse_requirements(requirements_file, session="hack"):
-            try:
-                # check markers, such as
-                #
-                #     rope_py3k    ; python_version >= '3.0'
-                #
-                if req.match_markers():
-                    requirements.append(str(req.req))
-            except AttributeError:
-                # pip >= 20.0.2
-                requirements.append(req.requirement)
-    return requirements
+readme_path = os.path.join(os.path.dirname(__file__), "README.rst")
+with open(readme_path, "r") as fp:
+    readme_text = fp.read()
 
 
-if __name__ == "__main__":
-    HERE = path.abspath(path.dirname(__file__))
-    INSTALL_REQUIRES = get_requirements(path.join(HERE, "requirements.txt"))
+version_for_setup_py = version.get_project_version("pghoard/version.py")
+version_for_setup_py = ".dev".join(version_for_setup_py.split("-", 2)[:2])
 
-    with io.open(path.join(HERE, "README.rst"), encoding="utf-8") as readme:
-        LONG_DESCRIPTION = readme.read()
 
-    def local_scheme(version):
-        """Skip the local version (eg. +xyz of 0.6.1.dev4+gdf99fe2)
-            to be able to upload to Test PyPI"""
-        return ""
-
-    setup(
-        name="modoboa-amavis",
-        description="The amavis frontend of Modoboa",
-        long_description=LONG_DESCRIPTION,
-        license="MIT",
-        url="http://modoboa.org/",
-        author="Antoine Nguyen",
-        author_email="tonio@ngyn.org",
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Environment :: Web Environment",
-            "Framework :: Django :: 2.2",
-            "Intended Audience :: System Administrators",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Topic :: Communications :: Email",
-            "Topic :: Internet :: WWW/HTTP",
+setup(
+    name="pghoard",
+    version=version_for_setup_py,
+    zip_safe=False,
+    packages=find_packages(exclude=["test"]),
+    install_requires=[
+        "cryptography",
+        "psycopg2 >= 2.0.0",
+        "pydantic",
+        "python-dateutil",
+        "python-snappy >= 0.5",
+        "requests >= 1.2.0",
+        "zstandard >= 0.11.1",
+    ],
+    extras_require={},
+    dependency_links=[],
+    package_data={},
+    entry_points={
+        "console_scripts": [
+            "pghoard = pghoard.pghoard:main",
+            "pghoard_archive_cleanup = pghoard.archive_cleanup:main",
+            "pghoard_archive_sync = pghoard.archive_sync:main",
+            "pghoard_create_keys = pghoard.create_keys:main",
+            "pghoard_gnutaremu = pghoard.gnutaremu:main",
+            "pghoard_postgres_command = pghoard.postgres_command:main",
+            "pghoard_restore = pghoard.restore:main",
         ],
-        keywords="email amavis spamassassin",
-        packages=find_packages(exclude=["docs", "test_project"]),
-        include_package_data=True,
-        zip_safe=False,
-        install_requires=INSTALL_REQUIRES,
-        use_scm_version={"local_scheme": local_scheme},
-        setup_requires=["setuptools_scm"],
-    )
+    },
+    author="Hannu Valtonen",
+    author_email="hannu.valtonen@ohmu.fi",
+    license="Apache 2.0",
+    platforms=["POSIX", "MacOS"],
+    description="PostgreSQL automatic backup/restore service daemon",
+    long_description=readme_text,
+    url="https://github.com/aiven/pghoard/",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Database :: Database Engines/Servers",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
