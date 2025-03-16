10ed05a462bc9406860fe0dc5da03e923d91f80e
wu <wu@.com>
2025-03-17 01:38:34 +0800

    getsentry/reload/action

--- CONTRIBUTING.rst
+++
@@ -1,1 +0,0 @@
-Please refer to the `Contribution Guidelines <https://github.com/openwisp/openwisp-notifications#contributing>`_.
--- openwisp_notifications/context_processors.py
+++
@@ -1,8 +0,0 @@
-from openwisp_notifications import settings as app_settings
-
-
-def notification_api_settings(request):
-    return {
-        'OPENWISP_NOTIFICATIONS_HOST': app_settings.OPENWISP_NOTIFICATIONS_HOST,
-        'OPENWISP_NOTIFICATIONS_SOUND': app_settings.OPENWISP_NOTIFICATIONS_SOUND,
-    }
