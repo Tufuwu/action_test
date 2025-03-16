7908bf23a52e02d6da26a9189e0b8fa57d713c0d
wu <wu@.com>
2025-03-16 18:59:11 +0800

    2gis/k8s-handle/action

--- setup.py
+++ setup.py
@@ -1,64 +1,31 @@
-# -*- coding: utf-8 -*-
-
-import io
+import os
 from setuptools import setup, find_packages
 
-readme = io.open('README.rst', encoding="utf-8").read()
-changes = io.open('CHANGELOG.rst', encoding="utf-8").read()
-version = '0.7.1'
+readme_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'README.md')
 
 
-def long_description():
-    """
-    return readme + changes, removing directive blocks that are only valid in the context
-    of sphinx doc"""
-
-    def remove_block(text, token, margin=0):
-        input_lines = text.splitlines()
-        for i, l in enumerate(input_lines):
-            if l.startswith(token):
-                break
-        start = i
-        end = input_lines.index("", start + margin)
-        return "\n".join(input_lines[:start] + input_lines[end:])
-
-    readme_ = remove_block(readme, ".. mermaid::", margin=2)
-    readme_ = remove_block(readme_, ".. autoclasstree::")
-    readme_ = remove_block(readme_, ".. autoclasstree::")
-    readme_ = remove_block(readme_, ".. versionchanged::")
-    return "{}\n\n{}".format(readme_, changes)
+def get_content(path):
+    with open(path, 'r') as f:
+        return f.read()
 
 
-setup(
-    name='sphinxcontrib-mermaid',
-    version=version,
-    url='https://github.com/mgaitan/sphinxcontrib-mermaid',
-    download_url='https://pypi.python.org/pypi/sphinxcontrib-mermaid',
-    license='BSD',
-    author=u'Martín Gaitán',
-    author_email='gaitan@gmail.com',
-    description='Mermaid diagrams in yours Sphinx powered docs',
-    long_description=long_description(),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Documentation',
-        'Topic :: Utilities',
-    ],
-    platforms='any',
-    packages=find_packages(),
-    include_package_data=True,
-    namespace_packages=['sphinxcontrib'],
-)
+setup(name='k8s-handle',
+      version=os.environ.get('RELEASE_TAG', '0.0.0'),
+      long_description=get_content(readme_path),
+      long_description_content_type='text/markdown',
+      description='Provisioning tool for Kubernetes apps',
+      url='http://github.com/2gis/k8s-handle',
+      author='Vadim Reyder',
+      author_email='vadim.reyder@gmail.com',
+      license='Apache 2.0',
+      packages=find_packages(exclude=("tests",)),
+      data_files=['requirements.txt'],
+      entry_points={
+          "console_scripts": [
+              "k8s-handle=k8s_handle:main",
+          ]
+      },
+      install_requires=get_content('requirements.txt').split('\n'),
+      zip_safe=False)
