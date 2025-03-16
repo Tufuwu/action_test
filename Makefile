7518be5536dd9ee8543a5156ba8d29447256cda9
wu <wu@.com>
2025-03-17 00:06:26 +0800

    fuhrysteve/marshmallow-jsonschema/action

--- Makefile
+++ Makefile
@@ -1,63 +1,61 @@
-PYTHON3 := $(shell which python3 2>/dev/null)
-COVERAGE3 := $(shell which coverage3 2>/dev/null)
+PROJECT = marshmallow_jsonschema
 
-PYTHON := python3
-COVERAGE := --cov=thewalrus --cov-report term-missing --cov-report=html:coverage_html_report --cov-report=xml:coverage.xml
-TESTRUNNER := -m pytest --randomly-seed=137 thewalrus
+PYTHON_VERSION ?= 3.8
+VIRTUAL_ENV ?= .venv
+PYTHON ?= $(VIRTUAL_ENV)/bin/python
 
-.PHONY: help
-help:
-	@echo "Please use \`make <target>' where <target> is one of"
-	@echo "  install            to install The Walrus"
-	@echo "  libperm            to compile the Fortran permanent library"
-	@echo "  wheel              to build the The Walrus wheel"
-	@echo "  dist               to package the source distribution"
-	@echo "  clean              to delete all temporary, cache, and build files"
-	@echo "  clean-docs         to delete all built documentation"
-	@echo "  test-cpp           to run the C++ test suite"
-	@echo "  test               to run the Python test suite"
-	@echo "  coverage           to generate a coverage report"
 
-.PHONY: install
-install:
-ifndef PYTHON3
-	@echo "To install The Walrus you need to have Python 3 installed"
-endif
-	$(PYTHON) setup.py install
+REQUIREMENTS = requirements.txt
+REQUIREMENTS_TEST = requirements-test.txt
+REQUIREMENTS_TOX = requirements-tox.txt
 
-.PHONY: wheel
-wheel:
-	$(PYTHON) setup.py bdist_wheel
+SHELL := /bin/bash -euo pipefail
 
-.PHONY: dist
-dist:
-	$(PYTHON) setup.py sdist
+venv_init:
+	pip install virtualenv
+	if [ ! -d $(VIRTUAL_ENV) ]; then \
+		virtualenv -p python$(PYTHON_VERSION) --prompt="($(PROJECT))" $(VIRTUAL_ENV); \
+	fi
 
-.PHONY : clean
-clean:
-	make -C examples clean
-	rm -rf thewalrus/__pycache__
-	rm -rf thewalrus/tests/__pycache__
-	rm -rf dist
-	rm -rf build
+venv:  venv_init
+	$(VIRTUAL_ENV)/bin/pip install -r $(REQUIREMENTS)
+	$(VIRTUAL_ENV)/bin/pip install -r $(REQUIREMENTS_TEST)
+	$(VIRTUAL_ENV)/bin/pip install -r $(REQUIREMENTS_TOX)
 
-doc:
-	make -C docs html
-
-.PHONY : clean-docs
-clean-docs:
-	rm -rf docs/libwalrus_cpp_api
-	make -C docs clean
-
-test-cpp:
-	make -C tests clean
-	echo "Going to compile C++ tests"
-	make -C tests cpptests
-	echo "Compilation done for C++ tests"
-	make -C tests
+tox:
+	tox
 
 test:
-	$(PYTHON) $(TESTRUNNER)
+	pytest
 
-coverage:
-	$(PYTHON) $(TESTRUNNER) $(COVERAGE)
+test_coverage:
+	pytest --cov-report html --cov-config .coveragerc --cov $(PROJECT)
+
+install_pandoc:
+	sudo apt install pandoc
+	pip install pypandoc
+
+sdist:
+	python setup.py sdist
+
+bdist_wheel:
+	pip install wheel
+	python setup.py bdist_wheel
+
+twine:
+	pip install twine
+
+pypitest: sdist bdist_wheel twine
+	twine upload -r pypitest dist/*
+
+pypi: sdist bdist_wheel twine
+	twine upload -r pypi dist/*
+
+
+clean_venv:
+	rm -rf $(VIRTUAL_ENV)
+
+clean_pyc:
+	find . -name \*.pyc -delete
+
+clean: clean_venv clean_pyc
--- setup.py
+++ setup.py
@@ -1,169 +1,71 @@
-# Copyright 2019 Xanadu Quantum Technologies Inc.
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#!/usr/bin/env python3
-import sys
 import os
-import platform
 
-from setuptools import find_packages
-
-
-with open("thewalrus/_version.py") as f:
-    version = f.readlines()[-1].split()[-1].strip("\"'")
+from setuptools import setup, find_packages
 
 
-requirements = [
-    "numpy",
-    "scipy>=1.2.1",
-    "numba>=0.49.1",
-    "dask[delayed]",
-    "sympy>=1.5.1",
-    "repoze.lru>=0.7"
-]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-setup_requirements = [
-    "numpy"
-]
+def read(fname):
+    with open(fname) as fp:
+        return fp.read()
 
-
-BUILD_EXT = True
 
 try:
-    import numpy as np
-    from numpy.distutils.core import setup
-    from numpy.distutils.extension import Extension
-except ImportError:
-    raise ImportError("ERROR: NumPy needs to be installed first. "
-                      "You can install it with pip:"
-                      "\n\npip install numpy")
+    import pypandoc
+
+    long_description = pypandoc.convert("README.md", "rst")
+except (IOError, ImportError, OSError):
+    long_description = read("README.md")
 
 
-if BUILD_EXT:
+REQUIREMENTS_FILE = "requirements.txt"
+REQUIREMENTS = open(os.path.join(PROJECT_DIR, REQUIREMENTS_FILE)).readlines()
 
-    USE_CYTHON = True
-    try:
-        from Cython.Build import cythonize
-        ext = 'pyx'
-    except:
-        def cythonize(x, compile_time_env=None):
-            return x
+REQUIREMENTS_TESTS_FILE = "requirements-test.txt"
+REQUIREMENTS_TESTS = open(
+    os.path.join(PROJECT_DIR, REQUIREMENTS_TESTS_FILE)
+).readlines()
 
-        USE_CYTHON = False
-        cythonize = cythonize
-        ext = 'cpp'
+REQUIREMENTS_TOX_FILE = "requirements-tox.txt"
+REQUIREMENTS_TOX = open(os.path.join(PROJECT_DIR, REQUIREMENTS_TOX_FILE)).readlines()
 
-
-    library_default = ""
-    USE_OPENMP = True
-    EIGEN_INCLUDE = [os.environ.get("EIGEN_INCLUDE_DIR", ""), "/usr/local/include/eigen3", "/usr/include/eigen3"]
-
-    LD_LIBRARY_PATH = os.environ.get('LD_LIBRARY_PATH', library_default).split(":")
-    C_INCLUDE_PATH = os.environ.get('C_INCLUDE_PATH', "").split(":") + [np.get_include()]  + EIGEN_INCLUDE + ["include"]
-
-    LD_LIBRARY_PATH = [i for i in LD_LIBRARY_PATH if i]
-    libraries = []
-
-    if platform.system() == 'Windows':
-        USE_OPENMP = False
-        cflags_default = "-static -O3 -Wall -fPIC"
-        extra_link_args_CPP = ["-std=c++14 -static", "-static-libgfortran", "-static-libgcc"]
-    elif platform.system() == 'Darwin':
-        cflags_default = "-O3 -Wall -fPIC -shared -Xpreprocessor -fopenmp -lomp -mmacosx-version-min=10.9"
-        libraries += ["omp"]
-        extra_link_args_CPP = ['-Xpreprocessor -fopenmp -lomp']
-        extra_include = ['/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/']
-        C_INCLUDE_PATH += ['/usr/local/opt/libomp/include']
-        LD_LIBRARY_PATH += ['/usr/local/opt/libomp/lib']
-    else:
-        cflags_default = "-O3 -Wall -fPIC -shared -fopenmp"
-        extra_link_args_CPP = ['-fopenmp']
-
-    CFLAGS = os.environ.get('CFLAGS', cflags_default).split() + ['-I{}'.format(np.get_include())]
-
-    USE_LAPACK = False
-    if os.environ.get("USE_LAPACK", ""):
-        USE_LAPACK = True
-        CFLAGS += [" -llapacke -DLAPACKE=1"]
-        libraries += ["lapacke"]
-        extra_link_args_CPP[0] += " -llapacke"
-
-    if os.environ.get("USE_OPENBLAS", ""):
-        USE_LAPACK = True
-        CFLAGS += [" -lopenblas -DLAPACKE=1"]
-        libraries += ["openblas"]
-        extra_link_args_CPP[0] += " -lopenblas"
-
-    extensions = cythonize([
-            Extension("libwalrus",
-                sources=["thewalrus/libwalrus."+ext,],
-                depends=["include/libwalrus.hpp",
-                         "include/eigenvalue_hafnian.hpp",
-                         "include/recursive_hafnian.hpp",
-                         "include/repeated_hafnian.hpp",
-                         "include/hafnian_approx.hpp",
-                         "include/torontonian.hpp",
-                         "include/permanent.hpp",
-                         "include/hermite_multidimensional.hpp",
-                         "include/stdafx.h",
-                         "include/fsum.hpp"],
-                include_dirs=C_INCLUDE_PATH,
-                library_dirs=['/usr/lib', '/usr/local/lib'] + LD_LIBRARY_PATH,
-                libraries=libraries,
-                language="c++",
-                extra_compile_args=["-std=c++11"] + CFLAGS,
-                extra_link_args=extra_link_args_CPP)
-    ], compile_time_env={'_OPENMP': USE_OPENMP, 'LAPACKE': USE_LAPACK})
-else:
-    extensions = []
-
-
-info = {
-    'name': 'thewalrus',
-    'version': version,
-    'maintainer': 'Xanadu Inc.',
-    'maintainer_email': 'nicolas@xanadu.ai',
-    'url': 'https://github.com/XanaduAI/thewalrus',
-    'license': 'Apache License 2.0',
-    'packages': find_packages(where="."),
-    'description': 'Open source library for hafnian calculation',
-    'long_description': open('README.rst').read(),
-    'provides': ["thewalrus"],
-    'install_requires': requirements,
-    'setup_requires': setup_requirements,
-    'ext_modules': extensions,
-    'ext_package': 'thewalrus'
+EXTRAS_REQUIRE = {
+    "enum": ["marshmallow-enum"],
+    "union": ["marshmallow-union"],
 }
 
-classifiers = [
-    "Development Status :: 3 - Alpha",
-    "Environment :: Console",
-    "Intended Audience :: Science/Research",
-    "License :: OSI Approved :: Apache Software License",
-    "Natural Language :: English",
-    "Operating System :: POSIX",
-    "Operating System :: MacOS :: MacOS X",
-    "Operating System :: POSIX :: Linux",
-    "Operating System :: Microsoft :: Windows",
-    "Programming Language :: Python",
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3 :: Only',
-    "Topic :: Scientific/Engineering :: Physics"
-]
 
-setup(classifiers=classifiers, **(info))
+setup(
+    name="marshmallow-jsonschema",
+    version="0.11.0",
+    description="JSON Schema Draft v4 (http://json-schema.org/)"
+    " formatting with marshmallow",
+    long_description=long_description,
+    author="Stephen Fuhry",
+    author_email="fuhrysteve@gmail.com",
+    url="https://github.com/fuhrysteve/marshmallow-jsonschema",
+    packages=find_packages(exclude=("test*",)),
+    package_dir={"marshmallow-jsonschema": "marshmallow-jsonschema"},
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    tests_require=REQUIREMENTS_TESTS + REQUIREMENTS_TOX,
+    extras_require=EXTRAS_REQUIRE,
+    license=read("LICENSE"),
+    zip_safe=False,
+    keywords=(
+        "marshmallow-jsonschema marshmallow schema serialization "
+        "jsonschema validation"
+    ),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    test_suite="tests",
+)
