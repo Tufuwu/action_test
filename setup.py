5a4ab0f3a52167526e7189891d8e4b8fccf4395e
wu <wu@.com>
2025-03-17 01:28:50 +0800

    p4lang/ptf/action

--- setup.py
+++ setup.py
@@ -1,54 +1,33 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-name = 'querycontacts'
-description = "Query network abuse contacts on the command-line for a given ip address on abuse-contacts.abusix.zone"
-cur_dir = os.path.abspath(os.path.dirname(__file__))
-
-# Read Version
-version_file = '%s/querycontacts/_version.py' % (cur_dir)
-with open(version_file) as f:
-    code = compile(f.read(), version_file, 'exec')
-    exec(code)
-
-with open(os.path.join(cur_dir, 'README.md')) as f:
-    long_description = f.read()
-
-with open('%s/requirements.txt' % cur_dir) as f:
-    requires = f.readlines()
+with open('README.md') as readme_file:
+    readme = readme_file.read()
 
 setup(
-    name=name,
-    version=__version__,
-    description=description,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    keywords = ['contact', 'query', 'dns', 'abuse contact', 'abuse', 'abusix', 'network'],
-    author='abusix',
-    author_email='fp@abusix.com',
-    python_requires='>=3',
-    url='https://github.com/abusix/querycontacts',
-    install_requires=requires,
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    scripts=['scripts/querycontacts'],
-    license="GNU General Public License v3 (GPLv3)",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Customer Service',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Security',
+    name='ptf',
+    version='0.9.1',
+    description='PTF is a Python based dataplane test framework.',
+    long_description=readme,
+    author='Antonin Bas',
+    author_email='antonin@barefootnetworks.com',
+    url='https://github.com/p4lang/ptf',
+    packages=[
+        'ptf', 'ptf.platforms',
     ],
-    project_urls={
-        'Source': 'https://github.com/abusix/querycontacts',
-        'Company': 'https://www.abusix.com/'
-    },
+    package_dir={'': 'src'},
+    scripts=[
+        'ptf',
+        'ptf_nn/ptf_nn_agent.py'
+    ],
+    zip_safe=False,
+    license='Apache License',
+    keywords='ptf',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ]
 )
