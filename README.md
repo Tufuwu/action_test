f72e45f7355cb28221d268ac34a9e48b961ff8a1
wu <wu@.com>
2025-03-17 00:18:57 +0800

    MrParosk/pyDenStream/action

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-# Ultra-Fast Force Fields (UF<sup>3</sup>)
+# pyDenStream
 
-[![Tests](https://github.com/sxie22/fast-linear-qmml/workflows/Tests/badge.svg)](https://github.com/sxie22/fast-linear-qmml/actions)
+![master](https://github.com/MrParosk/pyDenStream/workflows/master/badge.svg?branch=master) [![codecov](https://codecov.io/gh/MrParosk/pyDenStream/branch/master/graph/badge.svg?token=HEKMVIH5WO)](https://codecov.io/gh/MrParosk/pyDenStream)
 
-## Setup
-```bash
-conda create --name uf3_env python=3.7
-conda activate uf3_env
-git clone https://github.com/sxie22/uf3
-cd uf3
-pip install setuptools_scm wheel
-pip install -r requirements.txt
-pip install -e .
+Implementation of the algorithm [Density-Based Clustering over an Evolving Data Stream with Noise](https://archive.siam.org/meetings/sdm06/proceedings/030caof.pdf) in Python.
+
+## Installation
+
+```Shell
+pip install git+https://github.com/MrParosk/pyDenStream.git
 ```
-## Basic Usage
 
-## Citing This Work
+## Example
 
+An example of how to use this package is included in *examples/user_guide.ipynb*.
--- setup.py
+++ setup.py
@@ -1,36 +1,15 @@
-#!/usr/bin/env python
-
-import os
-import setuptools
+from setuptools import setup
 
 
-module_dir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(module_dir, 'README.md'), "r") as f:
-    long_description = f.read()
-
-install_requires = ['numpy',
-                    'scipy',
-                    'pandas',
-                    'ase>=3.20.1']
-test_requires = ['pytest']
+with open('requirements.txt') as file:
+    required = file.read().splitlines()
 
 
-if __name__ == "__main__":
-    setuptools.setup(
-        name='uf3',
-        version='0.1',
-        description='Ultra-Fast Force Fields for molecular dynamics',
-        long_description=long_description,
-        url='https://github.com/sxie22/uf3',
-        author='Stephen R. Xie, Matthias Rupp',
-        author_email='sxiexie@ufl.edu',
-        license='Apache 2.0',
-        packages=['uf3'],
-        install_requires=install_requires,
-        classifiers=["Programming Language :: Python :: 3.7",
-                     'Development Status :: 3 - Alpha',
-                     'Intended Audience :: Science/Research',
-                     'Operating System :: OS Independent',
-                     'Topic :: Scientific/Engineering'],
-        tests_require=test_requires,
-    )
+setup(name='denstream',
+      version='0.1',
+      description='Implementation of the DenStream algorithm',
+      author='MrParosk',
+      author_email='TBC',
+      packages=['denstream'],
+      install_requires=required,
+      )
