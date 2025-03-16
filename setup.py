1981a1e03cfed6a71c2e6ca1467faed489060a0e
wu <wu@.com>
2025-03-17 01:28:14 +0800

    abusix/querycontacts/action

--- setup.py
+++ setup.py
@@ -1,73 +1,54 @@
 import os
-from codecs import open
+from setuptools import setup, find_packages
 
-from setuptools import find_packages, setup
+name = 'querycontacts'
+description = "Query network abuse contacts on the command-line for a given ip address on abuse-contacts.abusix.zone"
+cur_dir = os.path.abspath(os.path.dirname(__file__))
 
-here = os.path.dirname(__file__)
+# Read Version
+version_file = '%s/querycontacts/_version.py' % (cur_dir)
+with open(version_file) as f:
+    code = compile(f.read(), version_file, 'exec')
+    exec(code)
 
-
-# Get the long description from the README file
-with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+with open(os.path.join(cur_dir, 'README.md')) as f:
     long_description = f.read()
 
-install_requires = [
-    "dask[array,dataframe]>=2.4.0",
-    "distributed>=2.4.0",
-    "numba",
-    "numpy>=1.17.3",
-    "pandas>=0.24.2",
-    "scikit-learn>=0.23",
-    "scipy",
-    "dask-glm>=0.2.0",
-    "multipledispatch>=0.4.9",
-    "packaging",
-]
-
-# Optional Requirements
-doc_requires = ["sphinx", "numpydoc", "sphinx-rtd-theme", "nbsphinx", "sphinx-gallery"]
-test_requires = [
-    "black",
-    "coverage",
-    "flake8",
-    "isort",
-    "pytest",
-    "pytest-cover",
-    "pytest-mock",
-]
-dev_requires = doc_requires + test_requires
-xgboost_requires = ["dask-xgboost", "xgboost"]
-complete_requires = xgboost_requires
-
-extras_require = {
-    "docs": doc_requires,
-    "test": test_requires,
-    "dev": dev_requires,
-    "xgboost": xgboost_requires,
-    "complete": complete_requires,
-}
+with open('%s/requirements.txt' % cur_dir) as f:
+    requires = f.readlines()
 
 setup(
-    name="dask-ml",
-    description="A library for distributed and parallel machine learning",
+    name=name,
+    version=__version__,
+    description=description,
     long_description=long_description,
-    url="https://github.com/dask/dask-ml",
-    author="Tom Augspurger",
-    author_email="taugspurger@anaconda.com",
-    license="BSD",
+    long_description_content_type="text/markdown",
+    keywords = ['contact', 'query', 'dns', 'abuse contact', 'abuse', 'abusix', 'network'],
+    author='abusix',
+    author_email='fp@abusix.com',
+    python_requires='>=3',
+    url='https://github.com/abusix/querycontacts',
+    install_requires=requires,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    scripts=['scripts/querycontacts'],
+    license="GNU General Public License v3 (GPLv3)",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Topic :: Database",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Customer Service',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Security',
     ],
-    packages=find_packages(exclude=["docs", "tests", "tests.*", "docs.*"]),
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    install_requires=install_requires,
-    extras_require=extras_require,
-    python_requires=">=3.6",
+    project_urls={
+        'Source': 'https://github.com/abusix/querycontacts',
+        'Company': 'https://www.abusix.com/'
+    },
 )
