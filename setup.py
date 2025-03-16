612cb6fca841fe3d84b73c2d43b773b4ef85b659
wu <wu@.com>
2025-03-17 02:03:50 +0800

    nmigen/nmigen-soc/action

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
-import io
-
 from setuptools import setup, find_packages
 
-# This reads the __version__ variable from openfermionpyscf/_version.py
-exec(open('openfermionpyscf/_version.py').read())
 
-# Readme file as long_description:
-long_description = io.open('README.rst', encoding='utf-8').read()
+def scm_version():
+    def local_scheme(version):
+        if version.tag and not version.distance:
+            return version.format_with("")
+        else:
+            return version.format_choice("+{node}", "+{node}.dirty")
+    return {
+        "relative_to": __file__,
+        "version_scheme": "guess-next-dev",
+        "local_scheme": local_scheme
+    }
 
-# Read in requirements.txt
-requirements = open('requirements.txt').readlines()
-requirements = [r.strip() for r in requirements]
 
 setup(
-    name='openfermionpyscf',
-    version=__version__,
-    author='The OpenFermion Developers',
-    author_email='help@openfermion.org',
-    url='http://www.openfermion.org',
-    description='A plugin allowing OpenFermion to interface with PySCF.',
-    long_description=long_description,
-    install_requires=requirements,
-    license='Apache 2',
-    packages=find_packages()
+    name="nmigen-soc",
+    use_scm_version=scm_version(),
+    author="whitequark",
+    author_email="whitequark@whitequark.org",
+    description="System on Chip toolkit for nMigen",
+    #long_description="""TODO""",
+    license="BSD",
+    setup_requires=["wheel", "setuptools", "setuptools_scm"],
+    install_requires=["nmigen>=0.2,<0.5"],
+    packages=find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/nmigen/nmigen-soc",
+        "Bug Tracker": "https://github.com/nmigen/nmigen-soc/issues",
+    },
 )
