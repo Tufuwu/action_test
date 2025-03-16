a5cf7d92442b137ed73199600cc71c2c74f84b20
wu <wu@.com>
2025-03-17 01:27:34 +0800

    dask/dask-ml/action

--- nautilus_terminal/__init__.py
+++
@@ -1,2 +0,0 @@
-VERSION = "3.4.0"
-APPLICATION_ID = "org.flozz.nautilus-terminal"
--- setup.py
+++ setup.py
@@ -1,70 +1,73 @@
-#!/usr/bin/env python
-# encoding: UTF-8
-
 import os
-from setuptools import setup, find_packages
-from setuptools.command.install import install as _install
+from codecs import open
 
-from nautilus_terminal import VERSION
+from setuptools import find_packages, setup
 
-class install(_install):
-    def run(self):
-        _install.run(self)
-
-        # Do what distutils install_data used to do... *sigh*
-        # Despite what the setuptools docs say, the omission of this
-        # in setuptools is a bug, not a feature.
-        print("== Installing Nautilus Python extension...")
-        src_file = "nautilus_terminal/nautilus_terminal_extension.py"
-        dst_dir = os.path.join(self.install_data, "share/nautilus-python/extensions")
-        self.mkpath(dst_dir)
-        dst_file = os.path.join(dst_dir, os.path.basename(src_file))
-        self.copy_file(src_file, dst_file)
-        print("== Done!")
-
-        print("== Installing GSettings Schema")
-        src_file = "./nautilus_terminal/schemas/org.flozz.nautilus-terminal.gschema.xml"
-        dst_dir = os.path.join(self.install_data, "share/glib-2.0/schemas")
-        self.mkpath(dst_dir)
-        dst_file = os.path.join(dst_dir, os.path.basename(src_file))
-        self.copy_file(src_file, dst_file)
-        print("== Done! Run 'glib-compile-schemas /usr/share/glib-2.0/schemas/' for a global installation to compile the schema.")
+here = os.path.dirname(__file__)
 
 
-long_description = ""
-if os.path.isfile("README.rst"):
-    long_description = open("README.rst", "r").read()
-elif os.path.isfile("README.md"):
-    long_description = open("README.md", "r").read()
+# Get the long description from the README file
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
 
+install_requires = [
+    "dask[array,dataframe]>=2.4.0",
+    "distributed>=2.4.0",
+    "numba",
+    "numpy>=1.17.3",
+    "pandas>=0.24.2",
+    "scikit-learn>=0.23",
+    "scipy",
+    "dask-glm>=0.2.0",
+    "multipledispatch>=0.4.9",
+    "packaging",
+]
+
+# Optional Requirements
+doc_requires = ["sphinx", "numpydoc", "sphinx-rtd-theme", "nbsphinx", "sphinx-gallery"]
+test_requires = [
+    "black",
+    "coverage",
+    "flake8",
+    "isort",
+    "pytest",
+    "pytest-cover",
+    "pytest-mock",
+]
+dev_requires = doc_requires + test_requires
+xgboost_requires = ["dask-xgboost", "xgboost"]
+complete_requires = xgboost_requires
+
+extras_require = {
+    "docs": doc_requires,
+    "test": test_requires,
+    "dev": dev_requires,
+    "xgboost": xgboost_requires,
+    "complete": complete_requires,
+}
 
 setup(
-    name="nautilus_terminal",
-    version=VERSION,
-    description="A terminal embedded in Nautilus, the GNOME's file browser",
-    url="https://github.com/flozz/nautilus-terminal",
-    license="GPL-3.0",
-
+    name="dask-ml",
+    description="A library for distributed and parallel machine learning",
     long_description=long_description,
-
-    author="Fabien LOISON",
-
-    keywords="nautilus extension terminal gnome",
-    platforms=["Linux", "BSD"],
-
-    packages=find_packages(),
-    include_package_data=True,
-
-    install_requires=[
-        "psutil~=5.2.2",
+    url="https://github.com/dask/dask-ml",
+    author="Tom Augspurger",
+    author_email="taugspurger@anaconda.com",
+    license="BSD",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-
-    extras_require={
-        "dev": [
-            "flake8",
-            "nox",
-        ],
-    },
-
-    cmdclass={"install": install}
+    packages=find_packages(exclude=["docs", "tests", "tests.*", "docs.*"]),
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    install_requires=install_requires,
+    extras_require=extras_require,
+    python_requires=">=3.6",
 )
