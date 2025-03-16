9f78fb5bee0d127881bc04ed5fdbcf20281ab2de
wu <wu@.com>
2025-03-16 21:42:58 +0800

    xflr6/gsheets/action

--- setup.py
+++ setup.py
@@ -1,25 +1,49 @@
+# setup.py
 
-#!/usr/bin/env python
-
-import os
-import sys
+import pathlib
 from setuptools import setup, find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
 setup(
-    name='vendorize',
-    version='0.2.1',
-    description='Vendorize packages from PyPI',
-    long_description=read("README.rst"),
-    author='Michael Williamson',
-    author_email='mike@zwobble.org',
-    url='http://github.com/mwilliamson/python-vendorize',
-    packages=find_packages(exclude=["tests", "tests.*", "examples", "examples.*"]),
-    scripts=["scripts/python-vendorize"],
-    keywords="vendor vendorize",
-    license="BSD-2-Clause",
+    name='gsheets',
+    version='0.6.dev0',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Pythonic wrapper for the Google Sheets API',
+    keywords='spreadhseets google api v4 wrapper csv pandas',
+    license='MIT',
+    url='https://github.com/xflr6/gsheets',
+    project_urls={
+        'Documentation': 'https://gsheets.readthedocs.io',
+        'Changelog': 'https://gsheets.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/xflr6/gsheets/issues',
+        'CI': 'https://github.com/xflr6/gsheets/actions',
+        'Coverage': 'https://codecov.io/gh/xflr6/gsheets',
+    },
+    packages=find_packages(),
+    platforms='any',
+    python_requires='>=3.6',
+    install_requires=[
+        'google-api-python-client',
+        'oauth2client>=1.5.0',
+    ],
+    extras_require={
+        'dev': ['tox>=3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['mock>=3', 'pytest>=4', 'pytest-mock>=2', 'pytest-cov'],
+        'docs': ['sphinx>=1.8', 'sphinx-rtd-theme'],
+    },
+    long_description=pathlib.Path('README.rst').read_text(encoding='utf-8'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
 )
-
