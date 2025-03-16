a2948e7d5e63ae450dc94bb07a9e642c882a076d
wu <wu@.com>
2025-03-16 21:57:23 +0800

    liormizr/s3path/action

--- setup.py
+++ setup.py
@@ -1,43 +1,37 @@
-# Use the newer `setuptools.setup()`, if available.
-try:
-    from setuptools import setup
-    kw = {
-        'test_suite': 'tests',
-        'tests_require': ['astunparse'],
-    }
-except ImportError:
-    from distutils.core import setup
-    kw = {}
+#!/usr/bin/env python
+import s3path
+from setuptools import setup
 
-setup(name='gast',  # gast, daou naer!
-      version='0.4.0',
-      packages=['gast'],
-      description='Python AST that abstracts the underlying Python version',
-      long_description='''
-A generic AST to represent Python2 and Python3's Abstract Syntax Tree(AST).
-
-GAST provides a compatibility layer between the AST of various Python versions,
-as produced by ``ast.parse`` from the standard ``ast`` module.''',
-      author='serge-sans-paille',
-      author_email='serge.guelton@telecom-bretagne.eu',
-      url='https://github.com/serge-sans-paille/gast/',
-      license="BSD 3-Clause",
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Console',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Natural Language :: English',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.4',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.7',
-                   'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9',
-                   'Programming Language :: Python :: 3.10',
-                   ],
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-      **kw
-      )
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+setup(
+    name=s3path.__name__,
+    version=s3path.__version__,
+    url='https://github.com/liormizr/s3path',
+    author='Lior Mizrahi',
+    author_email='li.mizr@gmail.com',
+    py_modules=['s3path'],
+    install_requires=[
+        'boto3>=1.16.35',
+        'smart-open',
+    ],
+    license='Apache 2.0',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    python_requires='>= 3.4',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
