0a51a28552ecec562d5d8d7aa38d3d1883d176f8
wu <wu@.com>
2025-03-16 22:39:59 +0800

    telegraphic/hickle/action

--- setup.py
+++ setup.py
@@ -1,56 +1,68 @@
-# Copyright 2017-2021 Lawrence Livermore National Security, LLC and other
-# Hatchet Project Developers. See the top-level LICENSE file for details.
-#
-# SPDX-License-Identifier: MIT
+# To increment version
+# Check you have ~/.pypirc filled in
+# git tag x.y.z
+# git push && git push --tags
+# rm -rf dist; python setup.py sdist bdist_wheel
+# TEST: twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# twine upload dist/*
 
-from setuptools import setup
-from setuptools import Extension
 from codecs import open
-from os import path
+import re
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import setup, find_packages
+import sys
 
-# Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+author = "Danny Price, Ellert van der Velden and contributors"
 
-# Get the version in a safe way which does not refrence hatchet `__init__` file
-# per python docs: https://packaging.python.org/guides/single-sourcing-package-version/
-version = {}
-with open("./hatchet/version.py") as fp:
-    exec(fp.read(), version)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+with open("requirements.txt", 'r') as fh:
+    requirements = fh.read().splitlines()
 
-setup(
-    name="hatchet",
-    version=version["__version__"],
-    description="A Python library for analyzing hierarchical performance data",
-    url="https://github.com/hatchet/hatchet",
-    author="Abhinav Bhatele",
-    author_email="bhatele@cs.umd.edu",
-    license="MIT",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-    ],
-    keywords="",
-    packages=[
-        "hatchet",
-        "hatchet.readers",
-        "hatchet.util",
-        "hatchet.external",
-        "hatchet.tests",
-        "hatchet.cython_modules.libs",
-    ],
-    install_requires=["pydot", "PyYAML", "matplotlib", "numpy", "pandas"],
-    ext_modules=[
-        Extension(
-            "hatchet.cython_modules.libs.reader_modules",
-            ["hatchet/cython_modules/reader_modules.c"],
-        ),
-        Extension(
-            "hatchet.cython_modules.libs.graphframe_modules",
-            ["hatchet/cython_modules/graphframe_modules.c"],
-        ),
-    ],
+with open("requirements_test.txt", 'r') as fh:
+    test_requirements = fh.read().splitlines()
+
+# Read the __version__.py file
+with open('hickle/__version__.py', 'r') as f:
+    vf = f.read()
+
+# Obtain version from read-in __version__.py file
+version = re.search(r"^_*version_* = ['\"]([^'\"]*)['\"]", vf, re.M).group(1)
+
+setup(name='hickle',
+      version=version,
+      description='Hickle - an HDF5 based version of pickle',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author=author,
+      author_email='dan@thetelegraphic.com',
+      url='http://github.com/telegraphic/hickle',
+      download_url=('https://github.com/telegraphic/hickle/archive/v%s.zip'
+                    % (version)),
+      platforms='Cross platform (Linux, Mac OSX, Windows)',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved',
+          'Natural Language :: English',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Utilities',
+          ],
+      keywords=['pickle', 'hdf5', 'data storage', 'data export'],
+      install_requires=requirements,
+      tests_require=test_requirements,
+      python_requires='>=3.5',
+      packages=find_packages(),
+      zip_safe=False,
 )
