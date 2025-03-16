eb7cccc55e77a603b0ea0a6217cfa392680f8a53
wu <wu@.com>
2025-03-17 01:45:30 +0800

    django/daphne/action

--- setup.py
+++ setup.py
@@ -1,89 +1,47 @@
-##############################################################################
-#
-# Copyright (c) 2008-2013 Agendaless Consulting and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the BSD-like license at
-# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
-# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
-# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
-# FITNESS FOR A PARTICULAR PURPOSE
-#
-##############################################################################
+import os
 
 from setuptools import find_packages, setup
 
+from daphne import __version__
 
-def readfile(name):
-    with open(name) as f:
-        return f.read()
-
-
-README = readfile("README.rst")
-CHANGES = readfile("CHANGES.txt")
-
-install_requires = [
-    "pyramid>=1.4",
-    "pyramid_mako>=0.3.1",  # lazy configuration loading works
-    "repoze.lru",
-    "Pygments",
-]
-
-extra_requires = [
-    "ipaddress",
-]
-
-testing_extras = [
-    "WebTest",
-    "nose",
-    "coverage",
-]
-
-docs_extras = [
-    "Sphinx >= 1.7.5",
-    "pylons-sphinx-themes >= 0.3",
-]
+# We use the README as the long_description
+readme_path = os.path.join(os.path.dirname(__file__), "README.rst")
+with open(readme_path) as fp:
+    long_description = fp.read()
 
 setup(
-    name="pyramid_debugtoolbar",
-    version="4.6.1",
-    description=(
-        "A package which provides an interactive HTML debugger "
-        "for Pyramid application development"
-    ),
-    long_description=README + "\n\n" + CHANGES,
+    name="daphne",
+    version=__version__,
+    url="https://github.com/django/daphne",
+    author="Django Software Foundation",
+    author_email="foundation@djangoproject.com",
+    description="Django ASGI (HTTP/WebSocket) server",
+    long_description=long_description,
+    license="BSD",
+    zip_safe=False,
+    package_dir={"twisted": "daphne/twisted"},
+    packages=find_packages() + ["twisted.plugins"],
+    include_package_data=True,
+    install_requires=["twisted[tls]>=18.7", "autobahn>=0.18", "asgiref~=3.2"],
+    setup_requires=["pytest-runner"],
+    extras_require={
+        "tests": ["hypothesis==4.23", "pytest~=3.10", "pytest-asyncio~=0.8"]
+    },
+    entry_points={
+        "console_scripts": ["daphne = daphne.cli:CommandLineInterface.entrypoint"]
+    },
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP :: WSGI",
-        "License :: Repoze Public License",
+        "Topic :: Internet :: WWW/HTTP",
     ],
-    keywords="wsgi pylons pyramid transaction",
-    author=(
-        "Chris McDonough, Michael Merickel, Casey Duncan, " "Blaise Laflamme"
-    ),
-    author_email="pylons-discuss@googlegroups.com",
-    url="https://docs.pylonsproject.org/projects/pyramid-debugtoolbar/en/latest/",  # noqa E501
-    license="BSD",
-    packages=find_packages("src", exclude=["tests"]),
-    package_dir={"": "src"},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
-    extras_require={
-        ':python_version<"3.3"': extra_requires,
-        "testing": testing_extras,
-        "docs": docs_extras,
-    },
-    test_suite="tests",
 )
