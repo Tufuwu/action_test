aabe2e6490315888e17e8c22f73ba4dee2b8f799
wu <wu@.com>
2025-03-17 00:23:47 +0800

    rm-hull/luma.emulator/action

--- setup.py
+++ setup.py
@@ -1,79 +1,94 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2015-2019 Bitergia
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Authors:
-#     Santiago Dueñas <sduenas@bitergia.com>
-#     Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
-#
 
-import codecs
-import os.path
 import re
-
-from setuptools import setup
-
-here = os.path.abspath(os.path.dirname(__file__))
-readme_md = os.path.join(here, 'README.md')
-version_py = os.path.join(here, 'sirmordred', '_version.py')
-
-# Get the package description from the README.md file
-with codecs.open(readme_md, encoding='utf-8') as f:
-    long_description = f.read()
-
-with codecs.open(version_py, 'r', encoding='utf-8') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
+import os
+import sys
+from io import open
+from setuptools import setup, find_packages
 
 
-setup(name="sirmordred",
-      description="Drive GrimoireLab tools to produce a dashboard",
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url="https://github.com/chaoss/grimoirelab-sirmordred",
-      version=version,
-      author="CHAOSS",
-      author_email="acs@bitergia.com",
-      license="GPLv3",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development',
-          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-          'Programming Language :: Python :: 3'
-      ],
-      keywords="development repositories analytics git github bugzilla jira jenkins",
-      packages=[
-          'sirmordred'
-      ],
-      install_requires=[
-          'grimoire-elk>=0.32',
-          'kidash>=0.4.13',
-          'grimoirelab-panels>=0.0.7',
-          'sortinghat>=0.7',
-          'elasticsearch==6.3.1',
-          'elasticsearch-dsl==6.3.1',
-          'file-read-backwards==2.0.0',
-          'colorlog==4.1.0'
-      ],
-      scripts=[
-          'bin/sirmordred',
-          'utils/healthcheck.py',
-          'utils/micro.py',
-          'utils/panels_config.py'
-      ],
-      zip_safe=False)
+def read_file(fname, encoding='utf-8'):
+    with open(os.path.join(os.path.dirname(__file__), fname), encoding=encoding) as r:
+        return r.read()
+
+
+def find_version(*file_paths):
+    fpath = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = read_file(fpath)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+
+    err_msg = 'Unable to find version string in {}'.format(fpath)
+    raise RuntimeError(err_msg)
+
+
+README = read_file('README.rst')
+CONTRIB = read_file('CONTRIBUTING.rst')
+CHANGES = read_file('CHANGES.rst')
+version = find_version('luma', 'emulator', '__init__.py')
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+test_deps = [
+    'pytest',
+    'pytest-cov',
+    'pytest-timeout'
+]
+
+setup(
+    name="luma.emulator",
+    version=version,
+    author="Richard Hull",
+    author_email="richard.hull@destructuring-bind.org",
+    description=("A suite of pseudo-devices for luma.core components"),
+    long_description="\n\n".join([README, CONTRIB, CHANGES]),
+    long_description_content_type="text/x-rst",
+    license="MIT",
+    keywords="raspberry orange banana pi rpi opi sbc oled lcd led display screen spi i2c emulator",
+    url="https://github.com/rm-hull/luma.emulator",
+    download_url="https://github.com/rm-hull/luma.emulator/tarball/" + version,
+    namespace_packages=["luma"],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"luma.emulator.images": [
+        "luma/emulator/images/led_on.png",
+        "luma/emulator/images/led_off.png",
+        "luma/emulator/images/7-segment.png"
+    ]},
+    install_requires=[
+        "luma.core>=1.14.0",
+        "pygame"
+    ],
+    setup_requires=pytest_runner,
+    tests_require=test_deps,
+    extras_require={
+        'docs': [
+            'sphinx >= 1.5.1'
+        ],
+        'qa': [
+            'rstcheck',
+            'flake8'
+        ],
+        'test': test_deps
+    },
+    zip_safe=False,
+    python_requires='>=3.6, <4',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Topic :: Education",
+        "Topic :: System :: Hardware",
+        "Topic :: System :: Hardware :: Hardware Drivers",
+        "Topic :: Software Development :: Libraries :: pygame",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ]
+)
