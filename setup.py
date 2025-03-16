d9f2c0f4b2b5431211ec52a5e80331652a357d03
wu <wu@.com>
2025-03-17 01:14:10 +0800

    aws-cloudformation/cloudformation-cli-python-plugin/action

--- setup.py
+++ setup.py
@@ -1,100 +1,71 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+"""Python 3.6 and 3.7 language support for the CloudFormation CLI"""
+import os.path
+import re
 
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+from setuptools import setup
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-
-# To use a consistent encoding
-from codecs import open
-from os import path
-from efficient_apriori import __version__
-
-VERSION = __version__
-
-here = path.abspath(path.dirname(__file__))
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 
-def read(fname):
-    return open(path.join(here, fname)).read()
+def read(*parts):
+    with open(os.path.join(HERE, *parts), "r", encoding="utf-8") as fp:
+        return fp.read()
+
+
+# https://packaging.python.org/guides/single-sourcing-package-version/
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
-    name="efficient_apriori",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version=VERSION,
-    description=" An efficient Python implementation of the Apriori \
-    algorithm.",
+    name="cloudformation-cli-python-plugin",
+    version=find_version("python", "rpdk", "python", "__init__.py"),
+    description=__doc__,
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    # The project's main homepage.
-    url="https://github.com/tommyod/Efficient-Apriori",
-    # Author details
-    author="tommyod",
-    author_email="tommy.odland@gmail.com",
-    # Choose your license
-    license="MIT",
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    author="Amazon Web Services",
+    author_email="aws-cloudformation-developers@amazon.com",
+    url="https://github.com/aws-cloudformation/aws-cloudformation-rpdk-python-plugin/",
+    # https://packaging.python.org/guides/packaging-namespace-packages/
+    packages=["rpdk.python"],
+    package_dir={"": "python"},
+    # package_data -> use MANIFEST.in instead
+    include_package_data=True,
+    zip_safe=True,
+    python_requires=">=3.6",
+    install_requires=[
+        "cloudformation-cli>=0.1.10,<0.2",
+        "docker>=3.7,<5",
+        "urllib3<1.26",
+    ],
+    entry_points={
+        "rpdk.v1.languages": [
+            "python37 = rpdk.python.codegen:Python37LanguagePlugin",
+            "python36 = rpdk.python.codegen:Python36LanguagePlugin",
+        ],
+        "rpdk.v1.parsers": [
+            "python37 = rpdk.python.parser:setup_subparser_python37",
+            "python36 = rpdk.python.parser:setup_subparser_python36",
+        ],
+    },
+    license="Apache License 2.0",
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         "Development Status :: 5 - Production/Stable",
-        # Indicate who your project is intended for
-        # 'Intended Audience :: End Users/Desktop',
-        # 'Intended Audience :: Healthcare Industry',
-        # Pick your license as you wish (should match "license" above)
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Code Generators",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
     ],
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(".", exclude=["contrib", "docs", "tests"]),
-    package_dir={"efficient_apriori": "efficient_apriori"},
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    # extras_require={
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # include_package_data=True,
-    package_data={"": ["templates/*", "*.tex", "*.html"]},
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #    'console_scripts': [
-    #         'sample=sample:main',
-    #    ],
-    # }
+    keywords="Amazon Web Services AWS CloudFormation",
 )
