75e9db78da629751a30398f542a6d7be13b66808
wu <wu@.com>
2025-03-16 20:14:07 +0800

    gabfl/mysql-batch/action

--- setup.py
+++ setup.py
@@ -1,49 +1,40 @@
-import os
-from distutils.core import setup
+from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
-    name='django-linkcheck',
-    version='1.8.1',
-    description="A Django app that will analyze and report on links in any "
-                "model that you register with it.",
-    long_description=read('README.rst'),
-    author='Andy Baker',
-    author_email='andy@andybak.net',
-    license='BSD',
-    url='https://github.com/DjangoAdminHackers/django-linkcheck',
-    packages=[
-        'linkcheck',
-        'linkcheck.management',
-        'linkcheck.management.commands',
-        'linkcheck.migrations',
-        'linkcheck.tests',
-        'linkcheck.tests.sampleapp',
-    ],
-    package_data={
-        'linkcheck': [
-            'templates/linkcheck/*.html',
-            'templates/linkcheck/*.xhtml',
-            'tests/media/*',
-        ]
+    name='mysql_batch',
+    version='1.2',
+    description='Run large MySQL UPDATE and DELETE queries with small batches to prevent table/row-level locks',
+    long_description=long_description,
+    author='Gabriel Bordeaux',
+    author_email='pypi@gab.lc',
+    url='https://github.com/gabfl/mysql-batch',
+    license='MIT',
+    packages=['mysql_batch'],
+    package_dir={'mysql_batch': 'src'},
+    install_requires=['pymysql', 'argparse'],  # external dependencies
+    entry_points={
+        'console_scripts': [
+            'mysql_batch = mysql_batch.mysql_batch:main',
+        ],
     },
-    install_requires=['requests'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+    classifiers=[  # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Topic :: Software Development',
+        'Topic :: Database',
+        'Topic :: Database :: Database Engines/Servers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Framework :: Django',
+        'Programming Language :: Python',
+        #  'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
     ],
 )
