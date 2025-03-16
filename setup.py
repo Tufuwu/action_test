89523a145ccaa2f200c6198323e61e2c57afe7b1
wu <wu@.com>
2025-03-16 22:55:16 +0800

    scikit-fuzzy/scikit-fuzzy/action

--- setup.py
+++ setup.py
@@ -1,73 +1,121 @@
-"""A setuptools based setup module.
+#! /usr/bin/env python
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
+descr = """scikit-fuzzy (a.k.a. `skfuzzy`): Fuzzy logic toolbox for Python.
+
+This package implements many useful tools for projects involving fuzzy logic,
+also known as grey logic.
 """
+
+DISTNAME            = 'scikit-fuzzy'
+DESCRIPTION         = 'Fuzzy logic toolkit for SciPy'
+LONG_DESCRIPTION    = descr
+MAINTAINER          = 'Joshua Warner'
+MAINTAINER_EMAIL    = 'joshua.dale.warner@gmail.com'
+LICENSE             = 'Modified BSD'
+URL                 = 'https://pypi.python.org/pypi/scikit-fuzzy'
+DOWNLOAD_URL        = 'https://github.com/scikit-fuzzy/scikit-fuzzy'
+
 import os
 import sys
 
-from setuptools import setup, find_packages
+import setuptools
+from distutils.command.build_py import build_py
 
-# pylint: disable=redefined-builtin
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
 
-here = os.path.abspath(os.path.dirname(__file__))  # pylint: disable=invalid-name
+# This is a bit (!) hackish: we are setting a global variable so that the main
+# skimage __init__ can detect if it is being loaded by the setup routine, to
+# avoid attempting to load components that aren't built yet:
+# the numpy distutils extensions that are used by scikit-image to recursively
+# build the compiled extensions in sub-packages is based on the Python import
+# machinery.
+builtins.__SKIMAGE_SETUP__ = True
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fid:
-    long_description = fid.read()  # pylint: disable=invalid-name
 
-with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as fid:
-    install_requires = [line for line in fid.read().splitlines() if line.strip()]
+with open('skfuzzy/__init__.py') as fid:
+    for line in fid:
+        if line.startswith('__version__'):
+            VERSION = line.strip().split()[-1][1:-1]
+            break
 
-# Please keep the meta information in sync with icontract/__init__.py.
-#
-# (mristin, 2020-10-09) We had to denormalize icontract_meta module (which
-# used to be referenced from setup.py and this file) since readthedocs had
-# problems with installing icontract through pip on their servers with
-# imports in setup.py.
-setup(
-    name='icontract',
-    # Don't forget to update the version in __init__.py and CHANGELOG.rst!
-    version='2.5.0',
-    description='Provide design-by-contract with informative violation messages.',
-    long_description=long_description,
-    url='https://github.com/Parquery/icontract',
-    author='Marko Ristin',
-    author_email='marko.ristin@gmail.com',
-    classifiers=[
-        # yapf: disable
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
-        # yapf: enable
-    ],
-    license='License :: OSI Approved :: MIT License',
-    keywords='design-by-contract precondition postcondition validation',
-    packages=find_packages(exclude=['tests']),
-    install_requires=install_requires,
-    extras_require={
-        'dev': [
-            # yapf: disable
-            'mypy==0.812',
-            'pylint==2.3.1',
-            'yapf==0.20.2',
-            'tox>=3.0.0',
-            'pydocstyle>=2.1.1,<3',
-            'coverage>=4.5.1,<5',
-            'docutils>=0.14,<1',
-            'pygments>=2.2.0,<3',
-            'dpcontracts==0.6.0',
-            'tabulate>=0.8.7,<1',
-            'py-cpuinfo>=5.0.0,<6',
-            'typeguard>=2,<3'
-            # yapf: enable
-        ] + (['deal==4.1.0'] if sys.version_info >= (3, 8) else []) + (['asyncstdlib==3.9.1']
-                                                                       if sys.version_info >= (3, 8) else []),
-    },
-    py_modules=['icontract'],
-    package_data={"icontract": ["py.typed"]},
-    data_files=[(".", ["LICENSE.txt", "README.rst", "requirements.txt"])])
+with open('DEPENDS.txt') as fid:
+    INSTALL_REQUIRES = []
+    for line in fid.readlines():
+        if line == '' or line[0] == '#' or line[0].isspace():
+            continue
+        INSTALL_REQUIRES.append(line.strip())
+
+# requirements for those browsing PyPI
+REQUIRES = [r.replace('>=', ' (>= ') + ')' for r in INSTALL_REQUIRES]
+REQUIRES = [r.replace('==', ' (== ') for r in REQUIRES]
+REQUIRES = [r.replace('[array]', '') for r in REQUIRES]
+
+
+def configuration(parent_package='', top_path=None):
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path)
+
+    config.set_options(
+        ignore_setup_xxx_py=True,
+        assume_default_configuration=True,
+        delegate_options_to_subpackages=True,
+        quiet=True)
+
+    config.add_subpackage('skfuzzy')
+
+    return config
+
+
+if __name__ == "__main__":
+    try:
+        from numpy.distutils.core import setup
+    except ImportError:
+        from setuptools import setup
+        extra = {}
+    else:
+        extra = {'configuration': configuration}
+
+    setup(
+        name=DISTNAME,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        license=LICENSE,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        version=VERSION,
+        package_data={
+            # Include saved test image
+            '': ['*.npy', '*.md', '*.txt'],
+        },
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Topic :: Scientific/Engineering',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Operating System :: MacOS'],
+
+        install_requires=INSTALL_REQUIRES,
+        requires=REQUIRES,
+        packages=setuptools.find_packages(exclude=['docs']),
+        include_package_data=True,
+        zip_safe=False,
+
+        cmdclass={'build_py': build_py},
+        **extra
+    )
