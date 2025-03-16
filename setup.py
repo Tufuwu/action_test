b51b4efe6aeb3db698d20c033cc4c9ba844c13b6
wu <wu@.com>
2025-03-16 20:57:19 +0800

    mathandy/svgpathtools/action

--- setup.py
+++ setup.py
@@ -1,45 +1,52 @@
 from setuptools import setup
+import codecs
+import os
 
 
-setup(
-        name='nose2pytest',
-        version='1.0.8',
-        packages=['nose2pytest'],
-        # py_modules=['assert_tools', 'nose2pytest'],
-        entry_points={
-            'console_scripts': [
-                'nose2pytest = nose2pytest.script:main',
+VERSION = '1.4.1'
+AUTHOR_NAME = 'Andy Port'
+AUTHOR_EMAIL = 'AndyAPort@gmail.com'
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    HERE = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+setup(name='svgpathtools',
+      packages=['svgpathtools'],
+      version=VERSION,
+      description=('A collection of tools for manipulating and analyzing SVG '
+                   'Path objects and Bezier curves.'),
+      long_description=read("README.md"),
+      long_description_content_type='text/markdown',
+      author=AUTHOR_NAME,
+      author_email=AUTHOR_EMAIL,
+      url='https://github.com/mathandy/svgpathtools',
+      # download_url = 'http://github.com/mathandy/svgpathtools/tarball/'+VERSION,
+      license='MIT',
+      install_requires=['numpy', 'svgwrite'],
+      platforms="OS Independent",
+      requires=['numpy', 'svgwrite'],
+      keywords=['svg', 'svg path', 'svg.path', 'bezier', 'parse svg path', 'display svg'],
+      classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Image Recognition",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            "Topic :: Scientific/Engineering :: Mathematics",
+            "Topic :: Scientific/Engineering :: Visualization",
+            "Topic :: Software Development :: Libraries :: Python Modules",
             ],
-            'pytest11': ['pytest_nose_assert_tools = nose2pytest.assert_tools'],
-        },
-        url='https://github.com/schollii/nose2pytest',
-        license='BSD-3',
-        author='Oliver Schoenborn',
-        author_email='oliver.schoenborn@gmail.com',
-        description='Convert nose.tools.assert_ calls found in your Nose test modules into raw asserts for pytest',
-        keywords='nose to pytest conversion',
-
-        python_requires='>=3.5',
-        classifiers=[
-            # How mature is this project? Common values are
-            #   3 - Alpha
-            #   4 - Beta
-            #   5 - Production/Stable
-            'Development Status :: 5 - Production/Stable',
-
-            # Indicate who your project is intended for
-            'Intended Audience :: Developers',
-            'Topic :: Software Development :: Testing',
-
-            # Pick your license as you wish (should match "license" above)
-            'License :: OSI Approved :: BSD License',
-
-            # Specify the Python versions you support here.
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3 :: Only',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-)
+      )
