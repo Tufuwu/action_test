144e6ec42ca441cd5880c42fe39d29cc602f8cc8
wu <wu@.com>
2025-03-16 19:19:59 +0800

    py-pa/django-minio-storage/action

--- setup.py
+++ setup.py
@@ -1,38 +1,35 @@
-import os
-from setuptools import setup, find_packages
+# encoding: utf-8
+from setuptools import setup
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(
-    name='click-man',
-    version='0.4.2',
-    url='https://github.com/click-contrib/click-man',
-    license='MIT',
-    description='Generate man pages for click based CLI applications',
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    author='Timo Furrer',
-    author_email='tuxtimo@gmail.com',
-    install_requires=[
-        'click',
-        'setuptools',
+    name="django-minio-storage",
+    license="MIT",
+    use_scm_version=True,
+    description="Django file storage using the minio python client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tom Houlé",
+    author_email="tom@kafunsho.be",
+    url="https://github.com/py-pa/django-minio-storage",
+    packages=[
+        "minio_storage",
+        "minio_storage/management/",
+        "minio_storage/management/commands/",
     ],
-    packages=find_packages(exclude=('tests', )),
-    entry_points={
-        'console_scripts': [
-            'click-man = click_man.__main__:cli',
-        ]
-    },
+    setup_requires=["setuptools_scm"],
+    install_requires=["django>=1.11", "minio>=4.0.21,<7"],
+    extras_require={"test": ["coverage", "requests"]},
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Documentation',
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
     ],
 )
