2217af9bc67d46fe34230cd06b278922c57cb4df
wu <wu@.com>
2025-03-17 00:07:50 +0800

    f-dangel/backpack/action

--- setup.py
+++ setup.py
@@ -1,48 +1,47 @@
+from os import path
+
 from setuptools import find_packages, setup
 
+# META
+##############################################################################
+AUTHORS = "F. Dangel, F. Kunstner"
+NAME = "backpack-for-pytorch"
+PACKAGES = find_packages()
+
+DESCRIPTION = "BackPACK: Packing more into backprop"
+LONG_DESCR = """
+    BackPACK is built on top of PyTorch.
+    It efficiently computes quantities other than the gradient.
+
+    Website: https://backpack.pt
+    Code: https://github.com/f-dangel/backpack
+    Documentation: https://readthedocs.org/projects/backpack/
+    Bug reports & feature requests: https://github.com/f-dangel/backpack/issues
+    """
+
+VERSION = "1.2.0"
+URL = "https://github.com/f-dangel/backpack"
+LICENSE = "MIT"
+
+# DEPENDENCIES
+##############################################################################
+REQUIREMENTS_FILE = "requirements.txt"
+REQUIREMENTS_PATH = path.join(path.abspath(__file__), REQUIREMENTS_FILE)
+
+with open(REQUIREMENTS_FILE) as f:
+    requirements = f.read().splitlines()
 
 setup(
-    name="bundlewrap",
-    version="4.3.0",
-    description="Config management with Python",
-    long_description=(
-        "By allowing for easy and low-overhead config management, BundleWrap fills the gap between complex deployments using Chef or Puppet and old school system administration over SSH.\n"
-        "While most other config management systems rely on a client-server architecture, BundleWrap works off a repository cloned to your local machine. It then automates the process of SSHing into your servers and making sure everything is configured the way it's supposed to be. You won't have to install anything on managed servers."
-    ),
-    author="Torsten Rehn",
-    author_email="torsten@rehn.email",
-    license="GPLv3",
-    url="http://bundlewrap.org",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            "bw=bundlewrap.cmdline:main",
-        ],
-    },
-    keywords=["configuration", "config", "management"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: System :: Installation/Setup",
-        "Topic :: System :: Systems Administration",
-    ],
-    install_requires=[
-        "cryptography",
-        "Jinja2",
-        "Mako",
-        "passlib",
-        "pyyaml",
-        "requests >= 1.0.0",
-        "tomlkit",
-    ],
+    author=AUTHORS,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCR,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    url=URL,
+    license=LICENSE,
+    packages=PACKAGES,
     zip_safe=False,
+    python_requires=">=3.6",
 )
