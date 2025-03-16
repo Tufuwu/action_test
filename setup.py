6d59d890108c46b9939c3253909e5af2cfc69106
wu <wu@.com>
2025-03-17 00:28:04 +0800

    dtcooper/python-fitparse/action

--- setup.py
+++ setup.py
@@ -1,49 +1,23 @@
-from setuptools import setup
-import ast
-import os
-import io
+from distutils.core import setup
+import sys
+
+import fitparse
 
 
-def version():
-    """Return version string."""
-    with open(os.path.join("curtsies", "__init__.py")) as input_file:
-        for line in input_file:
-            if line.startswith("__version__"):
-                return ast.parse(line).body[0].value.s
-
-
-def long_description():
-    with open("readme.md", encoding="utf-8") as f:
-        return f.read()
+requires = None
+if sys.version_info < (2, 7) or (3, 0) <= sys.version_info < (3, 3):
+    sys.exit("Python 2.7 or Python 3.3+ are required.")
 
 
 setup(
-    name="curtsies",
-    version=version(),
-    description="Curses-like terminal wrapper, with colored strings!",
-    long_description=long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/bpython/curtsies",
-    author="Thomas Ballinger",
-    author_email="thomasballinger@gmail.com",
-    license="MIT",
-    packages=["curtsies"],
-    install_requires=[
-        "blessings>=1.5",
-        "wcwidth>=0.1.4",
-    ],
-    tests_require=[
-        "pyte",
-        "pytest",
-    ],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-    ],
-    zip_safe=False,
+    name='fitparse',
+    version=fitparse.__version__,
+    description='Python library to parse ANT/Garmin .FIT files',
+    author='David Cooper',
+    author_email='dave@kupesoft.com',
+    url='https://www.github.com/dtcooper/python-fitparse',
+    license=open('LICENSE').read(),
+    packages=['fitparse'],
+    scripts=['scripts/fitdump'],  # Don't include generate_profile.py
+    install_requires=requires,
 )
