8c2be320a2642db14fee927c1b185b7f225c1a21
wu <wu@.com>
2025-03-16 23:35:12 +0800

    collective/collective.solr/action

--- setup.py
+++ setup.py
@@ -1,77 +1,88 @@
-"""setup.py
-   Installation script for payu
+from setuptools import setup
+from setuptools import find_packages
 
-   Additional configuration settings are in ``setup.cfg``.
-"""
+version = "8.4.2.dev0"
 
-import os
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-PKG_NAME = 'payu'
-PKG_VERSION = __import__(PKG_NAME).__version__
-PKG_PKGS = [path for (path, dirs, files) in os.walk(PKG_NAME)
-            if '__init__.py' in files]
-
-with open('README.rst') as f:
-    README_RST = f.read()
+long_description = "\n\n".join(
+    [open("README.rst").read(), open("CHANGES.rst").read()])
 
 setup(
-    name=PKG_NAME,
-    version=PKG_VERSION,
-    description='A climate model workflow manager for supercomputing '
-                'environments.',
-    long_description=README_RST,
-    author='Marshall Ward',
-    author_email='python@marshallward.org',
-    url='http://github.com/marshallward/payu',
-
-    packages=PKG_PKGS,
-    requires=[
-        'f90nml',
-        'PyYAML',
-        'requests',
-        'yamanifest',
-        'dateutil',
-        'tenacity',
-    ],
-    install_requires=[
-        'f90nml >= 0.16',
-        'yamanifest >= 0.3.4',
-        'PyYAML',
-        'requests[security]',
-        'python-dateutil',
-        'tenacity',
-    ],
-    tests_require=[
-        'pytest',
-        'pylint',
-        'Sphinx',
-    ],
-    entry_points={
-        'console_scripts': [
-            'payu = payu.cli:parse',
-            'payu-run = payu.subcommands.run_cmd:runscript',
-            'payu-collate = payu.subcommands.collate_cmd:runscript',
-            'payu-profile = payu.subcommands.profile_cmd:runscript',
-        ]
-    },
+    name="collective.solr",
+    version=version,
+    description="Solr integration for external indexing and searching.",
+    long_description=long_description,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="plone cmf zope indexing searching solr lucene",
+    author="Plone Community",
+    author_email="plone-developers@lists.sourceforge.net",
+    maintainer="Timo Stollenwerk",
+    maintainer_email="tisto@plone.org",
+    url="https://github.com/collective/collective.solr",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["collective"],
+    include_package_data=True,
+    platforms="Any",
+    zip_safe=False,
+    install_requires=[
+        "Acquisition",
+        "DateTime",
+        "Products.CMFCore",
+        "Products.CMFPlone >= 4.3.7",
+        "Products.GenericSetup",
+        "Products.ZCatalog",
+        # 'collective.indexing >= 2.1',
+        "collective.js.showmore",
+        "lxml",
+        "plone.app.content",
+        "plone.app.layout",
+        "plone.app.registry",
+        "plone.app.vocabularies",
+        "plone.api",
+        "plone.browserlayer",
+        "plone.indexer",
+        "plone.restapi",
+        "setuptools",
+        "transaction",
+        "zope.component",
+        "zope.i18nmessageid",
+        "zope.interface",
+        "zope.publisher",
+        "zope.schema",
     ],
     extras_require={
-        'mitgcm': ['mnctools>=0.2']
+        "test": ["plone.app.testing[robot]", "plone.app.robotframework[debug]"],
+        "test4": [
+            "Products.LinguaPlone >=3.1a1",
+            "plone.app.robotframework[debug]",
+            "plone.app.testing[robot]",
+        ],
     },
-    keywords='{0} supercomputer model climate workflow'.format(PKG_NAME)
+    entry_points="""
+      [z3c.autoinclude.plugin]
+      target=plone
+      [zopectl.command]
+      solr_activate=collective.solr.commands:solr_activate
+      solr_deactivate=collective.solr.commands:solr_deactivate
+      solr_clear_index=collective.solr.commands:solr_clear_index
+      solr_reindex=collective.solr.commands:solr_reindex
+    """,
 )
