dfdb80e8f86c815cadf2002cf6f6231361f9423b
wu <wu@.com>
2025-03-16 22:48:25 +0800

    wirepas/gateway/action

--- badges/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import BadgeWear
-
-
-class BadgeWearForm(ModelForm):
-
-    class Meta:
-        model = BadgeWear
-        fields = ('user', 'level')
