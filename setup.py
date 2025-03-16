8b3d670612826f0bd9506b9e0617e3158053fb52
wu <wu@.com>
2025-03-16 21:44:29 +0800

    carrotquest/django-clickhouse/action

--- setup.py
+++ setup.py
@@ -1,64 +1,27 @@
-from setuptools import setup, find_packages
-from distutils.core import Extension
-import os
-import re
+from setuptools import setup
 
-DISTNAME = 'mhkit'
-PACKAGES = find_packages()
-EXTENSIONS = []
-DESCRIPTION = 'Marine and Hydrokinetic Toolkit'
-AUTHOR = 'MHKiT developers'
-MAINTAINER_EMAIL = ''
-LICENSE = 'Revised BSD'
-URL = 'https://github.com/MHKiT-Software/mhkit-python'
-CLASSIFIERS=['Development Status :: 3 - Alpha',
-             'Programming Language :: Python :: 3',
-             'Topic :: Scientific/Engineering',
-             'Intended Audience :: Science/Research',
-             'Operating System :: OS Independent',
-            ]
-DEPENDENCIES = ['pandas>=1.0.0', 
-                'numpy<1.21.0', 
-                'scipy',
-                'matplotlib', 
-                'requests', 
-                'pecos>=0.1.9',
-                'fatpack',
-                'lxml',
-                'scikit-learn',
-		        'NREL-rex>=0.2.61',
-                'six>=1.13.0',
-                'netCDF4', 
-                'xarray']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-# use README file as the long description
-file_dir = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(file_dir, 'README.md'), encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
+requires = []
+with open('requirements.txt') as f:
+    for line in f.readlines():
+        line = line.strip()  # Remove spaces
+        line = line.split('#')[0]  # Remove comments
+        if line:  # Remove empty lines
+            requires.append(line)
 
-# get version from __init__.py
-with open(os.path.join(file_dir, 'mhkit', '__init__.py')) as f:
-    version_file = f.read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        VERSION = version_match.group(1)
-    else:
-        raise RuntimeError("Unable to find version string.")
-        
-setup(name=DISTNAME,
-      version=VERSION,
-      packages=PACKAGES,
-      ext_modules=EXTENSIONS,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      author=AUTHOR,
-      maintainer_email=MAINTAINER_EMAIL,
-      license=LICENSE,
-      url=URL,
-      classifiers=CLASSIFIERS,
-      zip_safe=False,
-      install_requires=DEPENDENCIES,
-      scripts=[],
-      include_package_data=True
-  )
+setup(
+    name='django-clickhouse',
+    version='1.0.3',
+    packages=['django_clickhouse'],
+    package_dir={'': 'src'},
+    url='https://github.com/carrotquest/django-clickhouse',
+    license='BSD 3-clause "New" or "Revised" License',
+    author='Carrot quest',
+    author_email='m1ha@carrotquest.io',
+    description='Django extension to integrate with ClickHouse database',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requires
+)
