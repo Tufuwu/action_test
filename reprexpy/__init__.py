5820e4dba36698759d8b1d704e0b851ef104aef5
wu <wu@.com>
2025-03-16 22:16:28 +0800

    bmwcarit/python-dlt/action

--- reprexpy/__init__.py
+++
@@ -1,2 +0,0 @@
-from .reprex import reprex, reprex_ex
-from .session_info import SessionInfo
--- setup.py
+++ setup.py
@@ -1,66 +1,30 @@
-import sys
-from os import path
+import setuptools
 
-from setuptools import setup
 
-install_requires = [
-    'pyperclip', 'asttokens', 'nbconvert<=6.0.1', 'nbformat', 'matplotlib', 'ipython',
-    'pyimgur', 'setuptools', 'stdlib-list', 'ipykernel', 'tornado'
-]
-
-is_v2 = sys.version_info[0] == 2
-is_low_v3 = sys.version_info[0] == 3 and sys.version_info[1] <= 4
-if is_v2:
-    install_requires.remove('ipython')
-    install_requires.append('ipython<6')
-    install_requires.remove('ipykernel')
-    install_requires.append('ipykernel<4.9')
-
-if is_v2 or is_low_v3:
-    install_requires.remove('matplotlib')
-    install_requires.append('matplotlib<3')
-
-# "Beginning with Matplotlib 3.1, Python 3.6 or above is required", so if we're
-# building against python 3.5, we have to require matplotlib < 3.1. otherwise
-# we'll get latest version of matplotlib which will throw error
-if sys.version_info[0] == 3 and sys.version_info[1] == 5:
-    install_requires.remove('matplotlib')
-    install_requires.append('matplotlib<3.1')
-
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
-    long_description = f.read()
-
-# Only include pytest-runner in setup_requires if we're invoking tests
-if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
-    setup_requires = ['pytest-runner']
-else:
-    setup_requires = []
-    
-setup(
-    name='reprexpy',
-    version='0.3.0',
-    description='Render reproducible examples of Python code (port of R '
-                'package `reprex`)',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Christopher Baker',
-    author_email='chriscrewbaker@gmail.com',
-    url='https://reprexpy.readthedocs.io/en/latest',
-    license='LICENSE.txt',
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+setuptools.setup(
+    name="dlt",
+    description="Python DLT implementation for DLT",
+    use_scm_version=True,
+    url="https://github.com/bmwcarit/python-dlt",
+    author="BMW Car IT",
+    license="MPL 2.0",
+    classifiers=[  # See:https://pypi.org/classifiers/
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Topic :: System :: Logging",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ],
-    packages=['reprexpy'],
-    install_requires=install_requires,
-    tests_require=['pytest', 'pyzmq', 'pickledb'],
-    setup_requires=setup_requires,
-    package_data={
-        'reprexpy': ['examples/*.py'],
-    }
+    keywords="dlt log trace testing",
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    install_requires=[],
+    zip_safe=False,
+    test_suite="tests",
+    entry_points={
+        'console_scripts': [
+            'py_dlt_receive = dlt.py_dlt_receive:main',
+        ],
+    },
 )
