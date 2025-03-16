cdc3be54788b102e68486d1d4c48f6a188371e18
wu <wu@.com>
2025-03-16 23:45:44 +0800

    lyft/cartography/action

--- setup.py
+++ setup.py
@@ -1,61 +1,68 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
-__version__ = "2.1.1"
+__version__ = '0.31.0'
+
 
 setup(
-    # package name in pypi
-    name="django-oscar-api",
-    # extract version from module.
+    name='cartography',
     version=__version__,
-    description="REST API module for django-oscar",
-    long_description=open("README.rst").read(),
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Unix",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-    ],
-    python_requires=">=3.6",
-    keywords="",
-    author="Lars van de Kerkhof, Martijn Jacobs",
-    author_email="lars@permanentmarkers.nl, martijn@devopsconsulting.nl",
-    url="https://github.com/django-oscar/django-oscar-api",
-    license="BSD",
-    packages=find_packages(
-        exclude=[
-            "*tests.unit",
-            "*tests.serializers*",
-            "*tests.doctests*",
-            "*fixtures",
-            "*fixtures*",
-            "*sandbox*",
-        ]
-    ),
-    # include non python files
-    include_package_data=True,
-    zip_safe=False,
-    # specify dependencies
-    install_requires=[
-        "setuptools",
-        "django-oscar>=3.0",
-        "Django>=2.2.13",  # CVE-2020-9402
-        "djangorestframework>=3.9",  # first version to support Django 2.2
-    ],
-    # mark test target to require extras.
-    extras_require={
-        "dev": ["coverage", "mock", "twine", "wheel", "easy_thumbnails"],
-        "lint": ["flake8", "flake8-black", "flake8-bugbear", "black>=19.10b0"],
+    description='Explore assets and their relationships across your technical infrastructure.',
+    url='https://www.github.com/lyft/cartography',
+    maintainer='Lyft',
+    maintainer_email='security@lyft.com',
+    license='apache2',
+    packages=find_packages(exclude=['tests*']),
+    package_data={
+        'cartography.data': [
+            '*.cypher',
+            '*.yaml',
+        ],
+        'cartography.data.jobs.analysis': [
+            '*.json',
+        ],
+        'cartography.data.jobs.cleanup': [
+            '*.json',
+        ],
     },
+    dependency_links=[],
+    install_requires=[
+        "boto3>=1.15.1",
+        "botocore>=1.18.1",
+        "dnspython>=1.15.0",
+        "neo4j>=1.7.6,<4.0.0",
+        "neobolt>=1.7.0,<4.0.0",
+        "policyuniverse>=1.1.0.0",
+        "google-api-python-client>=1.7.8",
+        "oauth2client>=4.1.3",
+        "marshmallow>=3.0.0rc7",
+        "okta<1.0.0",
+        "pyyaml>=5.3.1",
+        "requests>=2.22.0",
+        "statsd",
+        "packaging",
+    ],
+    extras_require={
+        ':python_version<"3.7"': [
+            "importlib-resources",
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'cartography = cartography.cli:main',
+            'cartography-detectdrift = cartography.driftdetect.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
