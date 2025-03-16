8115067da3fb0c170fbe8b1a1cc068edaba319d0
wu <wu@.com>
2025-03-17 00:12:06 +0800

    keiserlab/e3fp/action

--- setup.py
+++ setup.py
@@ -1,53 +1,63 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from os import path
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
+from setuptools import setup
+from e3fp import version
 
-with open(path.join(here, 'README.md')) as f:
-    long_description = f.read()
-
-from setuptools import setup, find_packages
+ON_RTD = os.environ.get("READTHEDOCS") == "True"
 
 
-with open('%s/VERSION' % here) as f:
-    __version__ = f.readline().strip()
+requirements = [
+    "scipy>=0.18.0",
+    "numpy>=1.11.3",
+    "mmh3>=2.3.1",
+    "sdaxen_python_utilities>=0.1.4",
+]
+if ON_RTD:  # ReadTheDocs can't handle C libraries
+    requirements = requirements[-1:] + ["mock"]
+
+test_requirements = ["pytest", "mock"]
+
+classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    "Operating System :: OS Independent",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "Topic :: Scientific/Engineering :: Chemistry",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+
+def get_readme():
+    with open("README.rst") as f:
+        return f.read()
+
 
 setup(
-    author='abusix',
-    author_email='fp@abusix.com',
-    description='ahocorapy - Pure python ahocorasick implementation',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    name='ahocorapy',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=['future'],
-    python_requires='>=2.7',
-    url='https://github.com/abusix/ahocorapy',
-    project_urls={
-        'Source': 'https://github.com/abusix/ahocorapy',
-        'Company': 'https://www.abusix.com/'
-    },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+    name="e3fp",
+    packages=[
+        "e3fp",
+        "e3fp.config",
+        "e3fp.conformer",
+        "e3fp.fingerprint",
+        "e3fp.test",
     ],
-    keywords = ['keyword', 'search', 'purepython', 'aho-corasick', 'ahocorasick', 'abusix'],
-    license='MIT',
-    version=__version__
+    version=version,
+    description="Molecular 3D fingerprinting",
+    long_description=get_readme(),
+    keywords="e3fp 3d molecule fingerprint conformer",
+    author="Seth Axen",
+    author_email="seth.axen@gmail.com",
+    license="LGPLv3",
+    url="https://github.com/keiserlab/e3fp",
+    classifiers=classifiers,
+    download_url="https://github.com/keiserlab/e3fp/tarball/" + version,
+    install_requires=requirements,
+    include_package_data=True,
+    tests_require=test_requirements,
 )
