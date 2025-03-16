fde26a0d5d97e8d4bf6f2b331bf1cfb2461dd145
wu <wu@.com>
2025-03-16 21:01:37 +0800

    matoking/protontricks/action

--- setup.py
+++ setup.py
@@ -1,61 +1,51 @@
-from setuptools import setup, find_packages
-import os
-import sys
-import version
+from setuptools import setup
 
 
-readme_path = os.path.join(os.path.dirname(__file__), "README.rst")
-with open(readme_path, "r") as fp:
-    readme_text = fp.read()
-
-
-version_for_setup_py = version.get_project_version("pglookout/version.py")
-version_for_setup_py = ".dev".join(version_for_setup_py.split("-", 2)[:2])
-
-
-requires = [
-    "psycopg2 >= 2.0.0",
-    "requests >= 1.2.0",
-]
-
-if sys.version_info[0] == 2:
-    requires.append("futures")
+DESCRIPTION = (
+    "A simple wrapper for running Winetricks commands for Proton-enabled "
+    "games."
+)
+LONG_DESCRIPTION = (
+    "A simple wrapper for running Winetricks commands for Proton-enabled "
+    "games. protontricks requires Winetricks."
+)
+AUTHOR = "Janne Pulkkinen"
+AUTHOR_EMAIL = "jannepulk@gmail.com"
+URL = "https://github.com/Matoking/protontricks"
 
 
 setup(
-    name="pglookout",
-    version=version_for_setup_py,
-    zip_safe=False,
-    packages=find_packages(exclude=["test"]),
-    install_requires=requires,
-    extras_require={},
-    dependency_links=[],
-    package_data={},
-    data_files=[],
+    name="protontricks",
+    use_scm_version={
+        "write_to": "src/protontricks/_version.py"
+    },
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    python_requires=">=3.5",
+    url=URL,
+    packages=["protontricks"],
+    package_data={"": ["LICENSE"]},
+    package_dir={"protontricks": "src/protontricks"},
+    setup_requires=["setuptools_scm"],
+    install_requires=["vdf>=3.2"],
     entry_points={
         "console_scripts": [
-            "pglookout = pglookout.pglookout:main",
-            "pglookout_current_master = pglookout.current_master:main",
-        ],
+            "protontricks = protontricks.cli:main"
+        ]
     },
-    author="Hannu Valtonen",
-    author_email="hannu.valtonen@aiven.io",
-    license="Apache 2.0",
-    platforms=["POSIX", "MacOS"],
-    description="PostgreSQL replication monitoring and failover daemon",
-    long_description=readme_text,
-    url="https://github.com/aiven/pglookout/",
+    include_package_data=True,
+    license="GPL3",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Database :: Database Engines/Servers",
-        "Topic :: Software Development :: Libraries",
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
 )
