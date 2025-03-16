1a6fc03d99f66289733f0cd221a87b55f731004d
wu <wu@.com>
2025-03-16 23:35:29 +0800

    danirus/django-comments-xtd/action

--- setup.py
+++ setup.py
@@ -1,88 +1,58 @@
-from setuptools import setup
-from setuptools import find_packages
+import sys
 
-version = "8.4.2.dev0"
+from setuptools import setup, find_packages
+from setuptools.command.test import test
 
-long_description = "\n\n".join(
-    [open("README.rst").read(), open("CHANGES.rst").read()])
+
+def run_tests(*args):
+    from django_comments_xtd.tests import run_tests
+    errors = run_tests()
+    if errors:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+test.run_tests = run_tests
+
 
 setup(
-    name="collective.solr",
-    version=version,
-    description="Solr integration for external indexing and searching.",
-    long_description=long_description,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
-        "Framework :: Plone :: 5.0",
-        "Framework :: Plone :: 5.1",
-        "Framework :: Plone :: 5.2",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "Intended Audience :: Other Audience",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-    keywords="plone cmf zope indexing searching solr lucene",
-    author="Plone Community",
-    author_email="plone-developers@lists.sourceforge.net",
-    maintainer="Timo Stollenwerk",
-    maintainer_email="tisto@plone.org",
-    url="https://github.com/collective/collective.solr",
-    license="GPL version 2",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    namespace_packages=["collective"],
+    name="django-comments-xtd",
+    version="2.8.2",
+    packages=find_packages(),
     include_package_data=True,
-    platforms="Any",
-    zip_safe=False,
+    license="MIT",
+    description=("Django Comments Framework extension app with thread "
+                 "support, follow up notifications and email "
+                 "confirmations."),
+    long_description=("A reusable Django app that extends django-contrib-"
+                      "comments Framework with thread support, following up "
+                      "notifications and comments that only hits the "
+                      "database after users confirm them by email."),
+    author="Daniel Rus Morales",
+    author_email="mbox@danir.us",
+    maintainer="Daniel Rus Morales",
+    maintainer_email="mbox@danir.us",
+    url="http://pypi.python.org/pypi/django-comments-xtd",
     install_requires=[
-        "Acquisition",
-        "DateTime",
-        "Products.CMFCore",
-        "Products.CMFPlone >= 4.3.7",
-        "Products.GenericSetup",
-        "Products.ZCatalog",
-        # 'collective.indexing >= 2.1',
-        "collective.js.showmore",
-        "lxml",
-        "plone.app.content",
-        "plone.app.layout",
-        "plone.app.registry",
-        "plone.app.vocabularies",
-        "plone.api",
-        "plone.browserlayer",
-        "plone.indexer",
-        "plone.restapi",
-        "setuptools",
-        "transaction",
-        "zope.component",
-        "zope.i18nmessageid",
-        "zope.interface",
-        "zope.publisher",
-        "zope.schema",
+        'Django>=2.2',
+        'django-contrib-comments>=1.9',
+        'djangorestframework>=3.9',
+        'docutils',
+        'six',
     ],
-    extras_require={
-        "test": ["plone.app.testing[robot]", "plone.app.robotframework[debug]"],
-        "test4": [
-            "Products.LinguaPlone >=3.1a1",
-            "plone.app.robotframework[debug]",
-            "plone.app.testing[robot]",
-        ],
-    },
-    entry_points="""
-      [z3c.autoinclude.plugin]
-      target=plone
-      [zopectl.command]
-      solr_activate=collective.solr.commands:solr_activate
-      solr_deactivate=collective.solr.commands:solr_deactivate
-      solr_clear_index=collective.solr.commands:solr_clear_index
-      solr_reindex=collective.solr.commands:solr_reindex
-    """,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+    ],
+    test_suite="dummy",
+    zip_safe=True
 )
