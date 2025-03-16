8685b9465221d2bddf23d549f0cf05e45681f84e
wu <wu@.com>
2025-03-16 23:34:01 +0800

    payu-org/payu/action

--- setup.py
+++ setup.py
@@ -1,39 +1,77 @@
-from setuptools import setup, find_packages
+"""setup.py
+   Installation script for payu
 
-version = '0.9.0'
-name = 'websockify'
-long_description = open("README.md").read() + "\n" + \
-    open("CHANGES.txt").read() + "\n"
+   Additional configuration settings are in ``setup.cfg``.
+"""
 
-setup(name=name,
-      version=version,
-      description="Websockify.",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3 :: Only",
-          "Programming Language :: Python :: 3.4"
-          "Programming Language :: Python :: 3.5"
-          "Programming Language :: Python :: 3.6"
-          "Programming Language :: Python :: 3.7"
-          "Programming Language :: Python :: 3.8"
-          "Programming Language :: Python :: 3.9"
-        ],
-      keywords='noVNC websockify',
-      license='LGPLv3',
-      url="https://github.com/novnc/websockify",
-      author="Joel Martin",
-      author_email="github@martintribe.org",
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-      packages=['websockify'],
-      include_package_data=True,
-      install_requires=['numpy'],
-      zip_safe=False,
-      entry_points={
+PKG_NAME = 'payu'
+PKG_VERSION = __import__(PKG_NAME).__version__
+PKG_PKGS = [path for (path, dirs, files) in os.walk(PKG_NAME)
+            if '__init__.py' in files]
+
+with open('README.rst') as f:
+    README_RST = f.read()
+
+setup(
+    name=PKG_NAME,
+    version=PKG_VERSION,
+    description='A climate model workflow manager for supercomputing '
+                'environments.',
+    long_description=README_RST,
+    author='Marshall Ward',
+    author_email='python@marshallward.org',
+    url='http://github.com/marshallward/payu',
+
+    packages=PKG_PKGS,
+    requires=[
+        'f90nml',
+        'PyYAML',
+        'requests',
+        'yamanifest',
+        'dateutil',
+        'tenacity',
+    ],
+    install_requires=[
+        'f90nml >= 0.16',
+        'yamanifest >= 0.3.4',
+        'PyYAML',
+        'requests[security]',
+        'python-dateutil',
+        'tenacity',
+    ],
+    tests_require=[
+        'pytest',
+        'pylint',
+        'Sphinx',
+    ],
+    entry_points={
         'console_scripts': [
-            'websockify = websockify.websocketproxy:websockify_init',
+            'payu = payu.cli:parse',
+            'payu-run = payu.subcommands.run_cmd:runscript',
+            'payu-collate = payu.subcommands.collate_cmd:runscript',
+            'payu-profile = payu.subcommands.profile_cmd:runscript',
         ]
-      },
-    )
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
+    extras_require={
+        'mitgcm': ['mnctools>=0.2']
+    },
+    keywords='{0} supercomputer model climate workflow'.format(PKG_NAME)
+)
