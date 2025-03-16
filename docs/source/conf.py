4b6a406ca71dbba85a92f15b20cc42d57671ef80
wu <wu@.com>
2025-03-16 22:11:29 +0800

    thp/urlwatch/action

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,105 +1,57 @@
-# -- Paths ------------------------------------------------
-import sys
-import os
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-sys.path.insert(0, os.path.abspath('../../qmeq/'))
-sys.path.insert(0, os.path.abspath('../..'))
+# -- Path setup --------------------------------------------------------------
 
-# -- General configuration ------------------------------------------------
-needs_sphinx = '1.3'
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.mathjax',
-              'sphinx.ext.napoleon'
-             ]
 
+# -- Project information -----------------------------------------------------
+
+project = 'urlwatch'
+copyright = '2021, Thomas Perl'
+author = 'Thomas Perl'
+
+# The full version, including alpha/beta/rc tags
+release = '2.23'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-#source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
-#source_encoding = 'utf-8-sig'
-master_doc = 'index'
 
-project = u'qmeq'
-copyright = u'2019, Gediminas Kirsanskas'
-author = u'Gediminas Kirsanskas'
-
-version = u'1.1'
-release = u'1.1'
-
-language = None
-#today = ''
-#today_fmt = '%B %d, %Y'
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-#default_role = None
-#add_function_parentheses = True
-#add_module_names = True
-#show_authors = False
-pygments_style = 'sphinx'
-#modindex_common_prefix = []
-#keep_warnings = False
-todo_include_todos = False
-
-# -- Options for HTML output ----------------------------------------------
-#html_theme = 'alabaster'
-#html_theme = 'classic'
-html_theme = 'sphinx_rtd_theme'
-#html_theme = 'sphinxdoc'
-#html_theme = 'epub'
-#html_theme_options = {}
-#html_theme_path = []
-#html_title = u'qmeq v0'
-#html_short_title = None
-#html_logo = None
-#html_favicon = None
-#html_static_path = ['_static']
-#html_extra_path = []
-#html_last_updated_fmt = None
-#html_use_smartypants = True
-#html_sidebars = {}
-#html_additional_pages = {}
-#html_domain_indices = True
-#html_use_index = True
-#html_split_index = False
-#html_show_sourcelink = True
-#html_show_sphinx = True
-#html_show_copyright = True
-#html_use_opensearch = ''
-#html_file_suffix = None
-#html_search_language = 'en'
-#html_search_options = {'type': 'default'}
-#html_search_scorer = 'scorer.js'
-htmlhelp_basename = 'qmeqdoc'
 
 
-# -- Options for LaTeX output ---------------------------------------------
-latex_elements = { }
-latex_documents = [
-    (master_doc, 'qmeq.tex', u'qmeq Documentation',
-     u'Gediminas Kirsanskas', 'manual'),
-]
-#latex_logo = None
-#latex_use_parts = False
-#latex_show_pagerefs = False
-#latex_show_urls = False
-#latex_appendices = []
-#latex_domain_indices = True
+# -- Options for HTML output -------------------------------------------------
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
 
-# -- Options for manual page output ---------------------------------------
-man_pages = [
-    (master_doc, 'qmeq', u'qmeq Documentation',
-     [author], 1)
-]
-#man_show_urls = False
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
 
-
-# -- Options for Texinfo output -------------------------------------------
-texinfo_documents = [
-    (master_doc, 'qmeq', u'qmeq Documentation',
-     author, 'qmeq', 'One line description of project.',
-     'Miscellaneous'),
-]
-#texinfo_appendices = []
-#texinfo_domain_indices = True
-#texinfo_show_urls = 'footnote'
-#texinfo_no_detailmenu = False
+master_doc = 'index'
