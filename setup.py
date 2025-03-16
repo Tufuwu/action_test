4a2034961cdd7e7c5557b703a45d5517b297ae82
wu <wu@.com>
2025-03-16 17:01:49 +0800

    jmozmoz/cloudmap/action

--- setup.py
+++ setup.py
@@ -1,37 +1,59 @@
-import io
+from __future__ import print_function
+# from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+from setuptools import setup
+
+from setuptools.command.install import install as _install
 import os
-from setuptools import setup, find_packages
+import versioneer
+import datetime
+from pathlib import Path
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-version = {}
-with io.open(os.path.join(PROJECT_ROOT, "src", "dirhash", "version.py")) as fp:
-    exec(fp.read(), version)
+def copy_config():
+    from shutil import copyfile, move
+    srcfiles = [
+        'CreateCloudMap.ini',
+    ]
+    srcfiles_overwrite = [
+    ]
 
-DESCRIPTION = 'Python module and CLI for hashing of file system directories.'
+    dstdir = os.path.expanduser('~/.CreateCloudMap')
+    Path(dstdir).mkdir(parents=True, exist_ok=True)
 
-try:
-    with io.open(os.path.join(PROJECT_ROOT, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except IOError:
-    long_description = DESCRIPTION
+    for src in srcfiles:
+        dstfile = os.path.join(dstdir, src)
+        srcpath = os.path.join('cfg', src)
+        if not os.path.exists(dstfile):
+            copyfile(srcpath, dstfile)
+        else:
+            copyfile(srcpath, dstfile + '.new')
+
+    bak_date = datetime.datetime.now().strftime(
+        "%Y%m%d%H%M%S")
+
+    for src in srcfiles_overwrite:
+        dstfile = os.path.join(dstdir, src)
+        srcpath = os.path.join('cfg', src)
+        if not os.path.exists(dstfile):
+            copyfile(srcpath, dstfile)
+        else:
+            move(dstfile, dstfile + '.bak.' + bak_date)
+            copyfile(srcpath, dstfile)
+
+
+class Install(_install):
+    def run(self):
+        _install.run(self)
+        copy_config()
+
+
+cmdclass = versioneer.get_cmdclass()
+cmdclass['install'] = Install
 
 setup(
-    name='dirhash',
-    version=version['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/andhus/dirhash-python',
-    author="Anders Huss",
-    author_email="andhus@kth.se",
-    license='MIT',
-    install_requires=['scantree>=0.0.1'],
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    entry_points={
-        'console_scripts': ['dirhash=dirhash.cli:main'],
-    },
-    tests_require=['pytest', 'pytest-cov']
+    version=versioneer.get_version(),
+    packages=['cloudmap', ],
+    cmdclass=cmdclass,
 )
