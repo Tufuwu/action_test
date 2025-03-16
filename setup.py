89654a04adffca18db107b5199dfacb5f7945c93
wu <wu@.com>
2025-03-16 21:53:47 +0800

    liqd/adhocracy4/action

--- setup.py
+++ setup.py
@@ -1,72 +1,31 @@
-# Copyright 2015-2020 Nir Cohen
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python3
 
-import codecs
-import os
-
+from setuptools import find_packages
 from setuptools import setup
 
-# The following version is parsed by other parts of this package.
-# Don't change the format of the line, or the variable name.
-package_version = "1.5.0"
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    # intentionally *not* adding an encoding option to open
-    return codecs.open(os.path.join(here, *parts), 'r').read()
-
-
-setup(
-    name='distro',
-    version=package_version,
-    url='https://github.com/python-distro/distro',
-    author='Nir Cohen',
-    author_email='nir36g@gmail.com',
-    license='Apache License, Version 2.0',
-    platforms='All',
-    description='Distro - an OS platform information API',
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    py_modules=['distro'],
-    entry_points={
-        'console_scripts': [
-            'distro = distro:main',
-        ]
-    },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: POSIX :: BSD',
-        'Operating System :: POSIX :: BSD :: FreeBSD',
-        'Operating System :: POSIX :: BSD :: MidnightBSD',
-        'Operating System :: POSIX :: BSD :: NetBSD',
-        'Operating System :: POSIX :: BSD :: OpenBSD',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Operating System',
-    ]
-)
+setup(name='adhocracy4',
+      version='0.0.0.dev1',
+      description='Adhocracy 4 core library',
+      license='AGPL-3+',
+      author='Liquid Democracy e.V.',
+      author_email='info@liqd.de',
+      url='https://liqd.net/en/software/',
+      packages=find_packages(exclude=['tests*']),
+      include_package_data=True,
+      install_requires=[
+          'bleach',
+          'Django >=1.8',
+          'django-allauth',
+          'django-autoslug',
+          'django-background-tasks',
+          'django-ckeditor',
+          'django-filter',
+          'django-widget-tweaks',
+          'djangorestframework >= 3.5, <4.0',
+          'easy-thumbnails',
+          'jsonfield',
+          'python-dateutil',
+          'python-magic',
+          'rules',
+      ],
+     )
