e145a01f3362ee194394123bffa99e69a99edb09
wu <wu@.com>
2025-03-16 21:18:50 +0800

    asmeurer/removestar/action

--- setup.py
+++ setup.py
@@ -1,38 +1,30 @@
-#!/usr/bin/env python
-from setuptools import setup
-from os.path import abspath, dirname, join
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-def read_relative_file(filename):
-    """Returns contents of the given file, whose path is supposed relative
-    to this module."""
-    with open(join(dirname(abspath(__file__)), filename)) as f:
-        return f.read()
+import versioneer
 
-
-setup(
-    name="django-typed-models",
-    description="""Sane single table model inheritance for Django""",
-    long_description=read_relative_file("README.rst"),
-    version=read_relative_file("VERSION").strip(),
-    author="Craig de Stigter",
-    author_email="craig@destigter.nz",
-    url="http://github.com/craigds/django-typed-models",
-    packages=["typedmodels"],
+setuptools.setup(
+    name="removestar",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Aaron Meurer",
+    author_email="asmeurer@gmail.com",
+    description="A tool to automatically replace 'import *' imports with explicit imports in files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.asmeurer.com/removestar/",
+    packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 3.1",
-        "Framework :: Django :: 3.2",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    entry_points={'console_scripts': [ 'removestar = removestar.__main__:main']},
+    python_requires= '>=3.6',
+    install_requires=[
+        'pyflakes'
+    ],
+    license='MIT',
 )
