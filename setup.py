fd7aced79b9b2b137876dab523d950c8d031a930
wu <wu@.com>
2025-03-16 20:42:28 +0800

    eht16/python-logstash-async/action

--- setup.py
+++ setup.py
@@ -1,61 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
+from os import path
 from setuptools import setup
+from shutil import rmtree
+import sys
 
-import os
+NAME = 'python-logstash-async'
+VERSION = '2.3.0'
 
-requirements_filename = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'requirements.txt')
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), 'rb') as f:
+    LONG_DESCRIPTION = f.read().decode('utf-8')
 
-with open(requirements_filename) as fd:
-    install_requires = [i.strip() for i in fd.readlines()]
 
-requirements_dev_filename = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'requirements-dev.txt')
+if 'bdist_wheel' in sys.argv:
+    # Remove previous build dir when creating a wheel build, since if files have been removed
+    # from the project, they'll still be cached in the build dir and end up as part of the
+    # build, which is really neat!
+    for directory in ('build', 'dist', 'python_logstash_async.egg-info'):
+        rmtree(directory, ignore_errors=True)
 
-with open(requirements_filename) as fd:
-    tests_require = [i.strip() for i in fd.readlines()]
-
-long_description_filename = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'README.md')
-
-with open(long_description_filename) as fd:
-    long_description = fd.read()
 
 setup(
-    name='fierce',
-    version='1.4.0',
-    description='A DNS reconnaissance tool for locating non-contiguous IP space.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/mschwager/fierce',
-    packages=['fierce'],
-    package_dir={'fierce': 'fierce'},
-    license='GPLv3',
+    name=NAME,
+    packages=['logstash_async'],
+    version=VERSION,
+    description='Asynchronous Python logging handler for Logstash.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/x-rst',
+    license='MIT',
+    author='Enrico Tröger',
+    author_email='enrico.troeger@uvena.de',
+    url='https://github.com/eht16/python-logstash-async',
+    project_urls={
+        'Travis CI': 'https://travis-ci.org/eht16/python-logstash-async/',
+        'Source code': 'https://github.com/eht16/python-logstash-async/',
+        'Documentation': 'https://python-logstash-async.readthedocs.io/en/stable/',
+    },
+    keywords='logging logstash asynchronous',
+    install_requires=['limits', 'pylogbeat', 'requests'],
+    python_requires='>3.5',
+    include_package_data=True,
     classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Security',
-    ],
-    install_requires=install_requires,
-    tests_require=tests_require,
-    python_requires='>=3.0',
-    entry_points={
-        'console_scripts': [
-            'fierce = fierce.fierce:main',
-        ],
-    },
-    package_data={
-        'fierce': [
-            'lists/*.txt',
-        ],
-    },
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Logging',
+    ]
 )
