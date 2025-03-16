feb5eec610b36fdf930f292e571b7181290d2af1
wu <wu@.com>
2025-03-17 00:30:23 +0800

    dobiasd/undictify/action

--- setup.py
+++ setup.py
@@ -1,77 +1,22 @@
-import os
-from setuptools import setup, find_packages
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-
-version = '1.6.2.dev0'
-
-long_description = (
-    read('README.rst') + '\n\n' +
-    read('CHANGES.rst') + '\n\n'
-    )
-
-
-setup(name='plone.rest',
-      version=version,
-      description="Plone support for HTTP verbs.",
-      long_description=long_description,
-      # Get more strings from
-      # https://pypi.org/classifiers/
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Plone :: 4.3",
-          "Framework :: Plone :: 5.0",
-          "Framework :: Plone :: 5.1",
-          "Framework :: Plone :: 5.2",
-          "Framework :: Plone :: Core",
-          "Framework :: Zope2",
-          "Framework :: Zope :: 4",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-      ],
-      keywords='rest http',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='https://github.com/plone/plone.rest/',
-      license='GPL version 2',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      extras_require=dict(
-          test=[
-              'plone.app.testing[robot]>=4.2.2',
-              'plone.app.robotframework',
-              'plone.dexterity',
-              'Products.CMFCore',
-              'requests',
-          ]
-      ),
-      install_requires=[
-          'setuptools',
-          'collective.monkeypatcher',
-          'zope.component',
-          'zope.interface',
-          'zope.publisher',
-          'zope.traversing',
-          'Products.CMFCore',
-          'Zope2',
-          'six',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setuptools.setup(
+    name="undictify",
+    version="0.10.0",
+    author="Tobias Hermann",
+    author_email="editgym@gmail.com",
+    description="Type-checked function calls at runtime",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/Dobiasd/undictify",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
