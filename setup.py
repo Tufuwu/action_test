1013872a8d8895c723a8a6a340d0a67f5a32044d
wu <wu@.com>
2025-03-17 02:01:30 +0800

    circus-tent/circus/action

--- setup.py
+++ setup.py
@@ -1,73 +1,61 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+from circus import __version__
 
-from setuptools import setup
+if not hasattr(sys, 'version_info') or sys.version_info < (3, 5, 0, 'final'):
+    raise SystemExit("Circus requires Python 3.5 or higher.")
 
-setup(
-    name="xbox-webapi",
-    version="1.1.8",
-    author="OpenXbox",
-    description="A library to authenticate with Windows Live/Xbox Live and use their API",
-    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
-    license="GPL",
-    keywords="xbox one live api",
-    url="https://github.com/OpenXbox/xbox-webapi-python",
-    packages=[
-        'xbox.webapi',
-        'xbox.webapi.common',
-        'xbox.webapi.scripts',
-        'xbox.webapi.api',
-        'xbox.webapi.api.provider',
-        'xbox.webapi.authentication'
-    ],
-    namespace_packages=['xbox'],
-    zip_safe=False,
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
-    ],
-    test_suite="tests",
-    install_requires=[
-        'requests',
-        'demjson',
-        'appdirs',
-        'urwid'
-    ],
-    setup_requires=['pytest-runner'],
-    tests_require=[
-        'pytest',
-        'betamax'
-    ],
-    extras_require={
-        'dev': [
-            'pip',
-            'bump2version',
-            'wheel',
-            'watchdog',
-            'flake8',
-            'tox',
+
+install_requires = ['psutil', 'pyzmq>=17.0', 'tornado>=5.0.2']
+
+try:
+    import argparse     # NOQA
+except ImportError:
+    install_requires.append('argparse')
+
+with open("README.md") as f:
+    README = f.read()
+
+
+setup(name='circus',
+      version=__version__,
+      packages=find_packages(exclude=["docs", "examples"]),
+      description=("Circus is a program that will let you run and watch "
+                   " multiple processes and sockets."),
+      long_description=README,
+      author="Mozilla Foundation & contributors",
+      author_email="services-dev@lists.mozila.org",
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "License :: OSI Approved :: Apache Software License"
+      ],
+      install_requires=install_requires,
+      extras_require={
+        'test': [
+            'nose',
+            'nose-cov',
             'coverage',
-            'Sphinx',
-            'twine',
-            'betamax',
-            'pytest',
-            'pytest-runner'
+            'mock',
+            'circus-web',
+            'gevent',
+            'papa',
+            'PyYAML',
+            'pyzmq>=17.0',
+            'flake8==2.1.0',
         ],
-    },
-    entry_points={
-        'console_scripts': [
-            'xbox-auth-via-browser=xbox.webapi.scripts.browserauth:main',
-            'xbox-authenticate=xbox.webapi.scripts.authenticate:main',
-            'xbox-auth-tui=xbox.webapi.scripts.tui:main',
-            'xbox-searchlive=xbox.webapi.scripts.search:main',
-            'xbox-change-gt=xbox.webapi.scripts.change_gamertag:main'
-        ]
-    }
-)
+      },
+      test_suite='circus.tests',
+      entry_points="""
+      [console_scripts]
+      circusd = circus.circusd:main
+      circusd-stats = circus.stats:main
+      circusctl = circus.circusctl:main
+      circus-top = circus.stats.client:main
+      circus-plugin = circus.plugins:main
+      """)
