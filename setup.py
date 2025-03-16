0a7507370cc80aaa9e7d27a6b2543bb66ab5fa93
wu <wu@.com>
2025-03-17 02:13:44 +0800

    mahjongrepository/mahjong/action

--- setup.py
+++ setup.py
@@ -1,52 +1,38 @@
-import os
-
 import setuptools
 
-
-def local_file(name: str) -> str:
-    """Interpret filename as relative to this file."""
-    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
+from distutils.core import setup
 
 
-SOURCE = local_file("src")
-README = local_file("README.md")
+with open('README.rst', 'r') as f:
+    readme = f.read()
 
-with open(local_file("src/hypothesis_jsonschema/__init__.py")) as o:
-    for line in o:
-        if line.startswith("__version__"):
-            _, __version__, _ = line.split('"')
+with open('CHANGELOG.rst', 'r') as f:
+    changelog = f.read()
 
-
-setuptools.setup(
-    name="hypothesis-jsonschema",
-    version=__version__,
-    author="Zac Hatfield-Dodds",
-    author_email="zac@hypothesis.works",
-    packages=setuptools.find_packages(SOURCE),
-    package_dir={"": SOURCE},
-    package_data={"": ["py.typed"]},
-    url="https://github.com/Zac-HD/hypothesis-jsonschema",
-    project_urls={"Funding": "https://github.com/sponsors/Zac-HD"},
-    license="MPL 2.0",
-    description="Generate test data from JSON schemata with Hypothesis",
-    zip_safe=False,
-    install_requires=["hypothesis>=5.3.0", "jsonschema>=3.0.2"],
-    python_requires=">=3.6",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Framework :: Hypothesis",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Education :: Testing",
-        "Topic :: Software Development :: Testing",
-        "Typing :: Typed",
+setup(
+    name='mahjong',
+    packages=[
+        'mahjong',
+        'mahjong.hand_calculating',
+        'mahjong.hand_calculating.yaku_list',
+        'mahjong.hand_calculating.yaku_list.yakuman',
     ],
-    long_description=open(README).read(),
-    long_description_content_type="text/markdown",
-    keywords="python testing fuzzing property-based-testing json-schema",
+    version='1.1.9',
+    description='Mahjong hands calculation',
+    long_description=readme + '\n\n' + changelog,
+    author='Alexey Lisikhin',
+    author_email='lisikhin@gmail.com',
+    url='https://github.com/MahjongRepository/mahjong',
+    data_files=[('', ['README.rst', 'CHANGELOG.rst'])],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
