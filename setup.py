4d49a813cf0d7c5bf27c2cf0d243b7ffc9a51c44
wu <wu@.com>
2025-03-16 21:23:46 +0800

    python-zk/kazoo/action

--- setup.py
+++ setup.py
@@ -1,72 +1,83 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# See LICENSE for more details.
-#
-# Copyright: Red Hat Inc. 2013-2014
-# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
-
-# pylint: disable=E0611
+import os
+import re
 from setuptools import setup, find_packages
-
-VERSION = open('VERSION', 'r').read().strip()
-
-
-def pre_post_plugin_type():
-    try:
-        from avocado.core.plugin_interfaces import JobPreTests as Pre
-        return 'avocado.plugins.result_events'
-    except ImportError:
-        return 'avocado.plugins.job.prepost'
+import sys
 
 
-if __name__ == "__main__":
-    setup(name='avocado-framework-plugin-vt',
-          version=VERSION,
-          description='Avocado Plugin for Virtualization Testing',
-          author='Avocado Developers',
-          author_email='avocado-devel@redhat.com',
-          url='http://github.com/avocado-framework/avocado-vt',
-          packages=find_packages(exclude=('selftests*',)),
-          include_package_data=True,
-          entry_points={
-              'console_scripts': [
-                  'avocado-runner-avocado-vt = avocado_vt.plugins.vt_runner:main',
-                  ],
-              'avocado.plugins.settings': [
-                  'vt-settings = avocado_vt.plugins.vt_settings:VTSettings',
-                  ],
-              'avocado.plugins.cli': [
-                  'vt-list = avocado_vt.plugins.vt_list:VTLister',
-                  'vt = avocado_vt.plugins.vt:VTRun',
-                  ],
-              'avocado.plugins.cli.cmd': [
-                  'vt-bootstrap = avocado_vt.plugins.vt_bootstrap:VTBootstrap',
-                  'vt-list-guests = avocado_vt.plugins.vt_list_guests:VTListGuests',
-                  'vt-list-archs = avocado_vt.plugins.vt_list_archs:VTListArchs',
-                  ],
-              pre_post_plugin_type(): [
-                  'vt-joblock = avocado_vt.plugins.vt_joblock:VTJobLock',
-                  ],
-              'avocado.plugins.init': [
-                  'vt-init = avocado_vt.plugins.vt_init:VtInit',
-                  ],
-              'avocado.plugins.resolver': [
-                  'avocado-vt = avocado_vt.plugins.vt_resolver:VTResolver'
-                  ],
-              'avocado.plugins.discoverer': [
-                  'avocado-vt = avocado_vt.plugins.vt_resolver:VTDiscoverer'
-                  ],
-              'avocado.plugins.runnable.runner': [
-                  'avocado-vt = avocado_vt.plugins.vt_runner:VTTestRunner',
-                  ],
-              },
-          install_requires=["netifaces", "simplejson", "six", "netaddr",
-                            "aexpect", "avocado-framework>=68.0"]
-          )
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.md')) as f:
+    CHANGES = f.read()
+version = ''
+with open(os.path.join(here, 'kazoo', 'version.py')) as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        f.read(), re.MULTILINE).group(1)
+
+PYPY = getattr(sys, 'pypy_version_info', False) and True or False
+
+install_requires = ['six']
+
+tests_require = install_requires + [
+    'mock',
+    'pytest',
+    'pytest-cov',
+    'flake8',
+    'objgraph',
+]
+
+if not PYPY:
+    tests_require += [
+        'gevent>=1.2',
+        'eventlet>=0.17.1',
+    ]
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    install_requires += [
+        'gevent>=1.2',
+        'eventlet>=0.17.1',
+        'pure-sasl',
+    ]
+
+setup(
+    name='kazoo',
+    version=version,
+    description='Higher Level Zookeeper Client',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Communications",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Networking",
+    ],
+    keywords='zookeeper lock leader configuration',
+    author="Kazoo team",
+    author_email="python-zk@googlegroups.com",
+    url="https://kazoo.readthedocs.io",
+    license="Apache 2.0",
+    packages=find_packages(),
+    test_suite="kazoo.tests",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+        'sasl': ['pure-sasl==0.5.1'],
+    },
+    long_description_content_type="text/markdown",
+)
