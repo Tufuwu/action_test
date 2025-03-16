5f43d7c3861daf47c99531a3be6370d9d87381d1
wu <wu@.com>
2025-03-16 23:32:35 +0800

    dorianbrown/rank_bm25/action

--- setup.py
+++ setup.py
@@ -1,5 +1,32 @@
-#!/usr/bin/env python
-
 from setuptools import setup
+import io
+import os
 
-setup()
+here = os.path.abspath(os.path.dirname(__file__))
+
+short_description = 'Various BM25 algorithms for document ranking'
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = short_description
+
+setup(
+    name='rank_bm25',
+    version='0.2.1',
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='D. Brown',
+    author_email='dorianstuartbrown@gmail.com',
+    url="https://github.com/dorianbrown/rank_bm25",
+    license='Apache2.0',
+    py_modules=['rank_bm25'],
+    install_requires=['numpy'],
+    extras_require={
+        'dev': [
+            'pytest'
+        ]
+    }
+)
